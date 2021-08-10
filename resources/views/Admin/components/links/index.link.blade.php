d650221e826decfb26bdfea9c4226c373dd681f1
Anderson de Souza Viana <contato.nandos@hotmail.com>
2021-08-10 08:28:51 -0300

    Create Components Links

---
+++ resources/views/Admin/components/links/index.link.blade.php
@@ -0,0 +1,29 @@
+@push('createEdit')
+    <!-- Plugin js-->
+    <script src="{{url(mix('admin/assets/libs/parsley.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/selectize.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/switchery.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/jquery.multi-select.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/select2.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/jquery.mockjax.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/jquery.autocomplete.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/jquery.bootstrap-touchspin.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/bootstrap-maxlength.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/flatpickr.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/spectrum.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/bootstrap-clockpicker.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/bootstrap-datepicker.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/jquery.mask.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/autoNumeric.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/dropzone.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/quill.min.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/libs/dropify.min.js'))}}"></script>
+
+    <!-- Pages init js-->
+    <script src="{{url(mix('admin/assets/js/pages/form-fileuploads.init.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/js/pages/add-product.init.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/js/pages/form-validation.init.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/js/pages/form-advanced.init.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/js/pages/form-pickers.init.js'))}}"></script>
+    <script src="{{url(mix('admin/assets/js/pages/form-masks.init.js'))}}"></script>
+@endpush
