d23238c22ef75d6187bcea78bf69dfef2b4b442f
Anderson de Souza Viana <contato.nandos@hotmail.com>
2021-09-17 07:27:13 -0300

    CRUD SEO FInalizado

--- config/modelsConfig.php
+++ config/modelsConfig.php
@@ -12,36 +12,36 @@ return [
     ],
 
     'InsertModelsMain' => (object) [
-        'Products' => (object) [
-            'PROD01' => (object)[
-                'ViewHome' => false,
-                'ViewListMenu' => true,
-                'IncludeCore' => [true, 3], // @param 1 boolean | @param 2 Int Limit
-                'config' => (object) [
-                    'titleMenu' => 'Produtos',
-                    'anchor' =>  false,
-                    'linkMenu' => 'prod01.page',
-                    'iconMenu' => 'mdi-home',
-                    'titlePanel' => 'Produtos',
-                    'iconPanel' => 'mdi-box'
-                ],
-                'IncludeSections' => (object) []
-            ],
-            'PROD02' => (object)[
-                'ViewHome' => false,
-                'ViewListMenu' => true,
-                'IncludeCore' => [true, 3], // @param 1 boolean | @param 2 Int Limit
-                'config' => (object) [
-                    'titleMenu' => 'Artigos',
-                    'anchor' =>  false,
-                    'linkMenu' => 'prod02.page',
-                    'iconMenu' => 'mdi-home',
-                    'titlePanel' => 'Artigos',
-                    'iconPanel' => 'mdi-box'
-                ],
-                'IncludeSections' => (object) []
-            ],
-        ]
+        // 'Products' => (object) [
+        //     'PROD01' => (object)[
+        //         'ViewHome' => false,
+        //         'ViewListMenu' => true,
+        //         'IncludeCore' => [true, 3], // @param 1 boolean | @param 2 Int Limit
+        //         'config' => (object) [
+        //             'titleMenu' => 'Produtos',
+        //             'anchor' =>  false,
+        //             'linkMenu' => 'prod01.page',
+        //             'iconMenu' => 'mdi-home',
+        //             'titlePanel' => 'Produtos',
+        //             'iconPanel' => 'mdi-box'
+        //         ],
+        //         'IncludeSections' => (object) []
+        //     ],
+        //     'PROD02' => (object)[
+        //         'ViewHome' => false,
+        //         'ViewListMenu' => true,
+        //         'IncludeCore' => [true, 3], // @param 1 boolean | @param 2 Int Limit
+        //         'config' => (object) [
+        //             'titleMenu' => 'Artigos',
+        //             'anchor' =>  false,
+        //             'linkMenu' => 'prod02.page',
+        //             'iconMenu' => 'mdi-home',
+        //             'titlePanel' => 'Artigos',
+        //             'iconPanel' => 'mdi-box'
+        //         ],
+        //         'IncludeSections' => (object) []
+        //     ],
+        // ]
     ],
 
     'Relations' => (object) [
--- resources/views/Client/pages/Products/PROD01/page.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('Client.Core.client')
-@section('content')
-
-@endsection
