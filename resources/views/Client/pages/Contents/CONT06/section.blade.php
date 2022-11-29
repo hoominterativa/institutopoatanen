1a7e1b6cf1204486489cd8779c0c2614712867b6
Rodrigo <rodrigoprofissional2014@gmail.com>
2022-11-29 17:08:09 -0300

    Edição 3

--- resources/views/Client/pages/Contents/CONT06/section.blade.php
+++ resources/views/Client/pages/Contents/CONT06/section.blade.php
@@ -1,7 +1,19 @@
 <section id="CONT06" class="cont06 container-fluid" style="background-image: url({{asset('storage/uploads/tmp/bg-section-gray.jpg')}})">
-    <div class="row justify-content-center align-content-center">
+    <div class="d-flex justify-content-center align-content-center">
         <div class="cont06__video">
-            <h4>Olá mkundo</h4>
+            <div class="content cont06__video__content">
+                <div id="videoApre" data-src="https://www.youtube.com/embed/j1eCMuvNbJk" data-capa-video="https://andreguimaraes.com.br/teste/painel/dados/fotos/institucional_2399601f.png" style="background-image: url(https://andreguimaraes.com.br/teste/painel/dados/fotos/institucional_2399601f.png);">
+                    <div class="engloba-player">
+                        <div class="play">
+                            <img class="trans-fast" src="https://andreguimaraes.com.br/teste/assets/images/play.png" alt="Play Vídeo">
+                       </div>
+                    </div>
+                </div>
+            </div>
+            <a href="{{route('cont06.page')}}" class="cont06__video__cta transition">
+                <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="" class="cont06__cta__icon me-3 transition">
+                CTA
+            </a>
         </div>
     </div>
 </section>
