5075e3ee3f71587a11746eb31c19c82137887155
Rodrigo <rodrigoprofissional2014@gmail.com>
2022-11-30 18:18:51 -0300

    Edição 5

--- resources/views/Client/pages/Topics/TOPI04/section.blade.php
+++ resources/views/Client/pages/Topics/TOPI04/section.blade.php
@@ -1,7 +1,57 @@
-<section id="ABOU01" class="abou01 container-fluid" style="background-image: url({{asset('storage/uploads/tmp/bg-section-gray.jpg')}})">
+<section id="TOPI04" class="topi04 container-fluid px-0">
     <div class="row">
-        .
+        <div class="topi04__boxLeft col-md-6 px-0">
+            <div class="topi04__image w-100 h-100">
+                <img src="{{asset('storage/uploads/tmp/slid01_path_image_desktop.png')}}" class="w-100 h-100"  alt="">
+            </div>
+        </div>
+        <div class="topi04__boxRight col-md-6 px-0">
+                <div class="topi04__top">
+                    <div class="topi04__description">
+                        <h4 class="topi04__destaque"></h4>
+                        <h2 class="topi04__title">Titulo</h2>
+                        <h3 class="topi04__subtitle">Subtitulo</h3>
+                        <div class="topi04__paragraph">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu eget purus mattis posuere. Donec tincidunt dignissim faucibus.  
+                            </p>
+                        </div>
+                        <a href="#" class="topi04__cta">CTA</a>
+                    </div>   
+                </div>
+                <div class="topi04__topics row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <div class="topi04__topics__item col">
+                        <div class="topi04__topics__image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="topi04__topics__description">
+                            <img src="" class="topi04__topics__icone" alt="">
+                            <h4 class="topi04__topics__title">Titulo do Destaque</h4>
+                        </div>
+                    </div>
+                    <div class="topi04__topics__item col">
+                        <div class="topi04__topics__image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="topi04__topics__description">
+                            <img src="" class="topi04__topics__icone" alt="">
+                            <h4 class="topi04__topics__title">Titulo do Destaque</h4>
+                        </div>
+                    </div>
+                    <div class="topi04__topics__item col">
+                        <div class="topi04__topics__image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="topi04__topics__description">
+                            <img src="" class="topi04__topics__icone" alt="">
+                            <h4 class="topi04__topics__title">Titulo do Destaque</h4>
+                        </div>
+                    </div>
+
+
+                </div>
+        </div>
     </div>
     {{-- END .row --}}
 </section>
-{{-- END #ABOU01 --}}
+{{-- END #ABOU04 --}}
