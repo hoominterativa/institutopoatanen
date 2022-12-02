baf812d3dbab1dae760dba5d607577a5b0ef2fa4
Rodrigo <rodrigoprofissiona2014@gmail.com>
2022-12-02 15:10:24 -0300

    Edição 4

--- resources/views/Client/pages/Feedbacks/FEED03/section.blade.php
+++ resources/views/Client/pages/Feedbacks/FEED03/section.blade.php
@@ -1,51 +1,51 @@
 <section id="FEED03" class="feed03 container-fluid">
     <div class="container container--feed03">
         <header class="feed03__header d-flex justify-content-center align-items-center">
-            <div class="feed03___image">
-                <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="">
+            <div class="feed03__header__image">
+                <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" class="w-100 h-100" alt="Ícone">
             </div>
-            <h4 class="feed03__texto d-flex flex-column align-align-items-end">
-                <span>Subtitulo</span>
-                <span>Titulo</span>
-            </h4>
+            <div class="feed03__header__texto d-flex flex-column align-align-items-end">
+                <h3 class="feed03__header__texto__subtitle">Subtitulo</h3>
+                <h2 class="feed03__header__texto__title">Titulo</h2>
+            </div>
         </header>
        <div class="feed03__content carrossel-feed03 owl-carousel">
-            <div class="feed03__item w-100">
-                <div class="feed03__item__item__top d-flex justify-content-center align-items-center">
-                    <div class="feed03__item__top__image">
-                        <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Imagem Perfil">
+            <div class="feed03__content__item w-100">
+                <div class="feed03__content__item__top d-flex justify-content-center align-items-center">
+                    <div class="feed03__content__item__top__image">
+                        <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" class="w-100 h-100" alt="Imagem Perfil">
                     </div>
-                    <h4 class="feed03__item__top__titulo">Nome do Usuário</h4>
+                    <h4 class="feed03__content__item__top__titulo">Nome do Usuário</h4>
                 </div>
-                <div class="feed03__item__description text-center">
+                <div class="feed03__content__item__description text-center">
                     <p>
                         Rutrum vitae turpis massa ullamcorper mauris adipiscing justo, imperdiet.
                         Vivamus et mi augue gravida commodo. Et, vulputate adipiscing velit praesent ultrices.
                     </p>
                 </div>
             </div>
-            <div class="feed03__item w-100">
-                <div class="feed03__item__item__top d-flex justify-content-center align-items-center">
-                    <div class="feed03__item__top__image">
-                        <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Imagem Perfil">
+            <div class="feed03__content__item w-100">
+                <div class="feed03__content__item__top d-flex justify-content-center align-items-center">
+                    <div class="feed03__content__item__top__image">
+                        <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" class="w-100 h-100" alt="Imagem Perfil">
                     </div>
-                    <h4 class="feed03__item__top__titulo">Nome do Usuário</h4>
+                    <h4 class="feed03__content__item__top__titulo">Nome do Usuário</h4>
                 </div>
-                <div class="feed03__item__description text-center">
+                <div class="feed03__content__item__description text-center">
                     <p>
                         Rutrum vitae turpis massa ullamcorper mauris adipiscing justo, imperdiet.
                         Vivamus et mi augue gravida commodo. Et, vulputate adipiscing velit praesent ultrices.
                     </p>
                 </div>
             </div>
-            <div class="feed03__item w-100">
-                <div class="feed03__item__item__top d-flex justify-content-center align-items-center">
-                    <div class="feed03__item__top__image">
-                        <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Imagem Perfil">
+            <div class="feed03__content__item w-100">
+                <div class="feed03__content__item__top d-flex justify-content-center align-items-center">
+                    <div class="feed03__content__item__top__image">
+                        <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" class="w-100 h-100" alt="Imagem Perfil">
                     </div>
-                    <h4 class="feed03__item__top__titulo">Nome do Usuário</h4>
+                    <h4 class="feed03__content__item__top__titulo">Nome do Usuário</h4>
                 </div>
-                <div class="feed03__item__description text-center">
+                <div class="feed03__content__item__description text-center">
                     <p>
                         Rutrum vitae turpis massa ullamcorper mauris adipiscing justo, imperdiet.
                         Vivamus et mi augue gravida commodo. Et, vulputate adipiscing velit praesent ultrices.
