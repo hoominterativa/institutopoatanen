84c34e3d4544e4a9733c1fce7ddccb6164c4a6f6
Rodrigo <rodrigoprofissional2014@gmail.com>
2022-12-06 10:00:29 -0300

    Edição 2

--- config/modelsClass.php
+++ config/modelsClass.php
@@ -105,6 +105,7 @@ return [
             'TOPI04' => (object)[
                 'controller' => App\Http\Controllers\Topics\TOPI04Controller::class,
                 'model' => App\Models\Topics\TOPI04Topics::class,
+            ],   
             'TOPI05' => (object)[
                 'controller' => App\Http\Controllers\Topics\TOPI05Controller::class,
                 'model' => App\Models\Topics\TOPI05Topics::class,
--- resources/views/Client/assets/js/base.js
+++ resources/views/Client/assets/js/base.js
@@ -1,7 +1,2 @@
 import '../../Core/Headers/HEAD02/src/main';
-import '../../pages/Abouts/ABOU02/src/main';
-import '../../pages/Contents/CONT06/src/main';
-import '../../pages/Topics/TOPI04/src/main';
-import '../../pages/Products/PROD02/src/main';
-import '../../pages/Feedbacks/FEED03/src/main';
 import '../../pages/Topics/TOPI05/src/main';
