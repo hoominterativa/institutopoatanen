44489c16e720f11c0078b2d001bc886fe7d5420e
frontrgomes <rodrigoprofissiona2014@gmail.com>
2022-12-09 17:21:11 -0300

    Edição 5

--- resources/views/Client/Core/Footers/FOOT03/app.blade.php
+++ resources/views/Client/Core/Footers/FOOT03/app.blade.php
@@ -1,4 +1,4 @@
-<div class="foot03 container-fluid px-0" style="background-color:#EFEFEF;">
+<div class="foot03 container-fluid px-0 position-relative" style="background-color:#EFEFEF;" >
     <a class="btn-comeBack">
         <img src="" alt="">
     </a>
@@ -13,7 +13,9 @@
                         </a>
                     </div>
                     <div class="foot03__top__logo mx-auto">
-                        <img src="{{asset('storage/uploads/tmp/logo.png')}}" class="w-100 h-100" alt="Logo">
+                        <a href="" rel="next">
+                            <img src="{{asset('storage/uploads/tmp/logo.png')}}" class="w-100 h-100" alt="Logo">
+                        </a>
                     </div>
                     <div class="foot03__top__redeSocial d-flex justify-content-between align-items-center">
                         <a href="#" rel="external">
@@ -32,9 +34,9 @@
                 </div>
             </div>
             <div class="foot03__bottom px-0">
-                <div class="row mx-0 w-100 justify-content-center">
-                    <div class="foot03__bottom__boxLeft col-md-6 row px-0">
-                        <nav class="foot03__bottom__boxLeft__nav  col-md-6 px-0 mx-0">
+                <div class="row mx-0 w-100 justify-content-between ">
+                    <div class="foot03__bottom__boxLeft col-sm-7 row px-0">
+                        <nav class="foot03__bottom__boxLeft__nav  col-sm-4 px-0 mx-0">
                             <h4 class="foot03__bottom__boxLeft__nav__title">Início</h4>
                             <ul class="px-0 mb-0">
                                 <li><a href="#" rel="next">Menu Tópicos</a></li>
@@ -44,7 +46,7 @@
                                 <li><a href="#" rel="next">Menu Tópicos</a></li>
                             </ul>
                         </nav>
-                        <nav class="foot03__bottom__boxLeft__nav col-md-6 px-0 mx-0">
+                        <nav class="foot03__bottom__boxLeft__nav col-sm-4 px-0 mx-0">
                             <h4 class="foot03__bottom__boxLeft__nav__title">Serviços</h4>
                             <ul class="px-0 mb-0">
                                 <li><a href="#" rel="next">Menu Tópicos</a></li>
@@ -55,10 +57,10 @@
                             </ul>
                         </nav>
                     </div>
-                    <div class="foot03__bottom__boxRight col-md-6 row px-0 mx-0">
-                        <nav class="foot03__bottom__boxRight__nav col-md-6 px-0">
+                    <div class="foot03__bottom__boxRight col-sm-5 row px-0 mx-0 justify-content-between">
+                        <nav class="foot03__bottom__boxRight__nav col-sm-5 px-0 w-auto">
                             <h4 class="foot03__bottom__boxRight__nav__title">Unidades</h4>
-                            <ul class="px-0">
+                            <ul class="px-0 mb-0">
                                 <li>
                                     <a href="#" rel="next" class="d-flex align-items-center">
                                         <div class="icone">
@@ -72,11 +74,11 @@
                                 </li>
                             </ul>
                         </nav>
-                        <nav class="foot03__bottom__boxRight__nav col-md-6 px-0">
+                        <nav class="foot03__bottom__boxRight__nav col-sm-5 px-0 w-auto">
                             <h4 class="foot03__bottom__boxRight__nav__title">Funcionamento</h4>
-                            <ul>
+                            <ul class="mb-0">
                                 <li>
-                                    <a href="#" rel="next">
+                                    <a href="#" rel="next" class="d-flex align-items-center">
                                         <div class="icone">
                                             <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="">
                                         </div>
@@ -95,8 +97,8 @@
     </div>
     <div class="foot03__credits d-flex justify-content-between align-items-center px-0">
         <div class="container px-0">
-            <div class="row">
-                <div class="d-flex justify-content-between w-100 px-0">
+            <div class="row row--mobile">
+                <div class="d-flex justify-content-between w-100 px-0 foot03__credits__content">
                     <nav class="foot03__credits__nav d-flex align-items-center">
                         <ul class="d-flex align-items-center justify-content-between px-0 mb-0">
                             <li><a href="#" rel="next">Privacidade</a></li>
@@ -106,7 +108,7 @@
                     </nav>
                     <div class="foot03__credits__logo">
                         <a href="#" rel="next">
-                            <img src="{{asset('storage/uploads/tmp/logo.png')}}" alt="Logo">
+                            <img src="{{asset('storage/uploads/tmp/logo.png')}}" class="w-100 h-100" alt="Logo">
                         </a>
                     </div>
                 </div>
