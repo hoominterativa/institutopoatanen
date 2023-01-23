ff114d53fd364a2235f783f0fd261cb2df70ec76
frontrgomes <rodrigoprofissiona2014@gmail.com>
2023-01-23 10:14:41 -0300

    Edição 2

--- resources/views/Client/pages/Slides/SLID02/section.blade.php
+++ resources/views/Client/pages/Slides/SLID02/section.blade.php
@@ -2,23 +2,14 @@
     <div class="SLID02 owl-carousel">
             <div class="container-slide container-fluid">
                 <a href="#" target="_blank" class="link-full"></a>
-                <img src="#" data-image-mobile="#" class="img-background-slide" alt="image Background">
+                <img src="slid01_path_image_png.png" data-image-mobile="#" class="img-background-slide" alt="image Background">
                 <div class="content-slide container ms-auto me-auto row align-items-center">
                     <div class="content-description col-12 col-lg-7">
-                            <h2>
-                                <span class="title">title</span>
-                                <span class="subtitle">subtitle</span>
-                            </h2>
-                            <p class="description">sadasdsadsad</p>
+                            <h2>title</h2>
+
                             <a href="#" target="_blank" class="btn-cta-slide py-2 px-4 transition">
-                                <div class="w-auto d-flex justify-content-center align-items-center">
-                                    <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" width="25px"/>
-                                    Euu
-                                </div>
+                                <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" width="25px"/>
                             </a>
-                    </div>
-                    <div class="img-floating-png col-lg-5">
-                            <img src="#" alt="image Destaque">
                     </div>
                 </div>
             </div>
