2f4457d45b60521f629f466b9c94c06926cc8438
Rodrigo <rodrigoprofissional2014@gmail.com>
2023-01-27 09:35:13 -0300

    Edição 2

--- resources/views/Client/pages/Portfolios/PORT101/section.blade.php
+++ resources/views/Client/pages/Portfolios/PORT101/section.blade.php
@@ -1,5 +1,25 @@
-<section class="topi05 container-fluid px-0">
-    <div class="container">
-
+<section id="PORT101" class="port101 container-fluid px-0">
+    <div class="container container--pd">
+        <header class="port101__emcompass text-center">
+            <h4 class="port101__emcompass__title">Título Tópico</h4>
+            <h5 class="port101__emcompass__subtitle">Subtitulo</h5>
+            <hr class="port101__emcompass__line"/>
+        </header>
+        <div class="port101__content">
+        {{-- <div class="port101__content carousel-port101 owl-carousel"> --}}
+            <div class="port101__content__box">
+                <div class="port101__content__box__image">
+                    <img src="" alt="">
+                </div>
+                <div class="port101__content__box__description">
+                    <h4 class="port101__content__box__description__title">Título Tópico</h4>
+                    <div class="port101__content__box__description__paragraph">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectur adipiscing elitLorem ipsum dolor sit ame
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
