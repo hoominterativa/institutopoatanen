5e08a386d8db3c3989ead709a1b34f51f7ff5001
frontrgomes <rodrigoprofissiona2014@gmail.com>
2023-01-31 17:57:33 -0300

    Edição 2

--- resources/views/Client/Core/Headers/HEAD03/app.blade.php
+++ resources/views/Client/Core/Headers/HEAD03/app.blade.php
@@ -1,84 +1,50 @@
-<nav>
-    <ul class="menu-list list-inline mb-0">
-        <li class="list-inline-item menu-item dropdown">
-            <a href="{{route('home')}}" class="link transition">Home</a>
-        </li>
-        @foreach ($listMenu as $module => $menu)
-            @if ($menu->viewer == 'list')
-                <li class="list-inline-item menu-item {{$menu->dropdown?'dropdown':''}}">
-                    @if ($menu->dropdown)
-                        @foreach ($menu->dropdown as $item)
-                            @if ($item->subList)
-                                <a href="{{$item->route}}" data-bs-toggle="dropdown" class="link transition">{{$item->name}} <i class="menu-arrow"></i></a>
-                                <div class="dropdown-menu">
-                                    @foreach ($item->subList as $subItem)
-                                        <a href="{{$subItem->route}}" class="sublink-item transition">{{$subItem->name}}</a>
-                                    @endforeach
-                                </div>
-                            @else
-                                <a href="{{$item->route}}" class="link transition {{!$menu->anchor?isActive($menu->link):''}}">{{$item->name}}</a>
-                            @endif
-                        @endforeach
-                    @endif
-                </li>
-            @else
-                <li class="list-inline-item menu-item {{$menu->dropdown?'dropdown':''}}">
-                    <a href="{{$menu->anchor?$menu->link:route($menu->link)}}" {{$menu->dropdown?'data-bs-toggle=dropdown':''}} {{$menu->anchor?'data-bs-toggle=jqueryanchor':''}} class="link transition {{!$menu->anchor?isActive($menu->link):''}}">
-                        {{$menu->title}}
-                        @if ($menu->dropdown)
-                            <i class="menu-arrow"></i>
-                        @endif
-                    </a>
-                    @if ($menu->dropdown)
-                        <div class="sublink--menu text-end dropdown-menu" aria-labelledby="sublink--menu" >
-                            @foreach ($menu->dropdown as $item)
-                                @if ($item->subList)
-                                    <div class="mb-2 {{$menu->dropdown?'dropdown':''}}">
-                                        <a href="{{$item->route}}" data-bs-toggle="dropdown" class="sublink-item transition">{{$item->name}} <i class="menu-arrow"></i></a>
-                                        <div class="dropdown-menu">
-                                            @foreach ($item->subList as $subItem)
-                                                <a href="{{$subItem->route}}" class="sublink-item transition">{{$subItem->name}}</a>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                @else
-                                    <a href="{{$item->route}}" class="sublink-item transition">{{$item->name}}</a>
-                                @endif
-                            @endforeach
-                        </div>
-                    @endif
-                </li>
-            @endif
-        @endforeach
-    </ul>
-</nav>
-
-@if (isset($linksCtaHeader))
-    @if ($linksCtaHeader->count() > 1)
-        <div class="dropdown">
-            <a href="javascript:void(0)" data-bs-toggle="dropdown" class="btn-cta">{{$linksCtaHeader['title']??''}} <i class="menu-arrow"></i></a>
-            <div class="sublink--sidebar-right text-end dropdown-menu" aria-labelledby="sublink--sidebar-right" >
-                @foreach ($linksCtaHeader as $title => $linkCtaHeader)
-                    @if ($title <> 'title')
-                        @if ($linkCtaHeader[1] == '_lightbox')
-                            <a href="{{$linkCtaHeader[0]}}" data-fancybox="" class="sublink-item transition mb-2">{{$title}}</a>
-                        @else
-                            <a href="{{$linkCtaHeader[0]}}" target="{{$linkCtaHeader[1]}}" class="sublink-item transition mb-2">{{$title}}</a>
-                        @endif
-                    @endif
-                @endforeach
+<div id="HEAD03" class="container-fluid">
+    <div class="container">
+        <div class="container-header d-flex align-items-center justify-content-between">
+            <div id="logoHeader">
+                <a href="#">
+                    <img src="{{asset('storage/uploads/tmp/logo.png')}}" alt="Logo">
+                </a>
             </div>
+            {{-- END #logoHeader --}}
+            <div class="menu-sidebar-header">
+                <div class="btn-menu-sidebar-header">
+                    <span>Menu</span>
+                    <a href="#menu02--sidebar-right" alt="#" nofollow data-plugin="sidebar" data-sb-position="right" class="d-flex align-items-center">
+                        <div class="lines">
+                            <i class="w-100 mb-2 mx-auto transition"></i>
+                            <i class="w-100 mb-2 mx-auto transition"></i>
+                            <i class="w-100 mb-0 mx-auto transition"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            {{-- END menu-sidebar-header --}}
         </div>
-    @else
-        @foreach ($linksCtaHeader as $title => $linkCtaHeader)
-            @if ($title <> 'title')
-                @if ($linkCtaHeader[1] == '_lightbox')
-                    <a href="{{$linkCtaHeader[0]}}" data-fancybox="" class="sublink-item transition mb-2">{{$title}}</a>
-                @else
-                    <a href="{{$linkCtaHeader[0]}}" target="{{$linkCtaHeader[1]}}" class="sublink-item transition mb-2">{{$title}}</a>
-                @endif
-            @endif
-        @endforeach
-    @endif
-@endif
-{{-- END .btn-cta --}}
+        {{-- END .container-header --}}
+    </div>
+    {{-- END .container --}}
+</div>
+{{-- END #HEAD02 --}}
+
+<div class="head3-flutuante container-fluid">
+    <div class="link-flutuante">
+        <a href="#">
+            <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Ícone Rede Social">
+        </a>
+        <a href="#">
+            <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Ícone Rede Social">
+        </a>
+        <a href="#">
+            <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Ícone Rede Social">
+        </a>
+        <a href="#">
+            <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Ícone Rede Social">
+        </a>
+    </div>
+    <nav class="d-flex align-items-center link-translate">
+        <a href="#" class="btn-translate px-2" alt="{{__('Traduzir para Inglês')}}">EN</a>
+        <a href="#" class="btn-translate px-2 border-0" alt="{{__('Traduzir para Portugês')}}">PT</a>
+    </nav>
+    {{-- END .link-translate --}}
+</div>
