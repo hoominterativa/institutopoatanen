f0f2ddfb9864f6202c25eaf24e31109dc877c6d6
Rodrigo <rodrigoprofissional2014@gmail.com>
2023-02-01 11:05:54 -0300

    adição de plugin de video

--- resources/views/Client/pages/Contents/CONT07/section.blade.php
+++ resources/views/Client/pages/Contents/CONT07/section.blade.php
@@ -1,1 +1,27 @@
-asdasdasdasdas2
+<section id="CONT07" class="cont07 container-fluid px-0" style="background-image: url({{asset('storage/uploads/tmp/bg-section-gray.jpg')}})">
+    <div class="container px-0 mx-auto">
+        <div class="cont07__emcompass text-center">
+            <h4 class="cont07__emcompass__title">Titulo</h4>
+            <h5 class="cont07__emcompass__subtitle">Subtitulo</h5>
+        </div>
+        <div class="cont07__content d-flex justify-content-center align-content-center">
+            <div class="cont07__boxVideo">
+                <div class="cont07__boxVideo__content">
+                    <div id="videoApre" class="cont07__boxVideo__content__video d-flex justify-content-center align-items-center" data-src="https://www.youtube.com/embed/H5v3kku4y6Q" data-capa-video="{{asset('storage/uploads/tmp/bg-section-dark-gray.jpg')}}" style="background-image: url({{asset('storage/uploads/tmp/bg-section-dark-gray.jpg')}});">
+                        <img class="trans-fast play" src="{{asset('storage/uploads/tmp/play.png')}}" alt="Play Vídeo">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cont07__gallery carousel-gallery">
+            <a class="cont07__gallery__item" href="#">
+                <img src="" />
+            </a>
+        </div>
+        <a href="#" class="cont07__cta transition d-flex justify-content-center align-items-center">
+            <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="" class="cont07__cta__icon me-3 transition">
+            CTA
+        </a>
+    </div>
+</section>
+{{-- END #cont07 --}}
