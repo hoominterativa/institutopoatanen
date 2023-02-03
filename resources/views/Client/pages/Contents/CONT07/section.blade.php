7ab4ad1f3bd92ab8bbbf55103e8d666c4f1d83af
Rodrigo <rodrigoprofissional2014@gmail.com>
2023-02-03 11:52:22 -0300

    tweak css and structure

--- resources/views/Client/pages/Contents/CONT07/section.blade.php
+++ resources/views/Client/pages/Contents/CONT07/section.blade.php
@@ -1,5 +1,5 @@
 <section id="CONT07" class="cont07 container-fluid px-0" style="background-image: url({{asset('storage/uploads/tmp/bg-section-gray.jpg')}})">
-    <div class="container px-0 mx-auto">
+    <div class="container container--pd px-0 mx-auto">
         <div class="cont07__emcompass text-center">
             <h4 class="cont07__emcompass__title">Titulo</h4>
             <h5 class="cont07__emcompass__subtitle">Subtitulo</h5>
@@ -7,16 +7,34 @@
         <div class="cont07__content d-flex justify-content-center align-content-center">
             <div class="cont07__boxVideo">
                 <div class="cont07__boxVideo__content">
-                    <div id="videoApre" class="cont07__boxVideo__content__video d-flex justify-content-center align-items-center" data-src="https://www.youtube.com/embed/H5v3kku4y6Q" data-capa-video="{{asset('storage/uploads/tmp/bg-section-dark-gray.jpg')}}" style="background-image: url({{asset('storage/uploads/tmp/bg-section-dark-gray.jpg')}});">
-                        <img class="trans-fast play" src="{{asset('storage/uploads/tmp/play.png')}}" alt="Play Vídeo">
+                        <a href="https://www.youtube.com/embed/H5v3kku4y6Q" class="play" data-fancybox>
+                            <img class="trans-fast " src="{{asset('storage/uploads/tmp/play.png')}}" alt="Play Vídeo">
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="cont07__gallery carousel-gallery">
-            <a class="cont07__gallery__item" href="#">
-                <img src="" />
-            </a>
+        <div class="cont07__gallery carousel-gallery-cont07 owl-carousel mx-auto">
+            <div class="cont07__gallery__item">
+                <a  href="https://www.youtube.com/embed/H5v3kku4y6Q" data-fancybox="galeria-video">
+                    <img src="{{asset('storage/uploads/tmp/logo-for.png')}}" />
+                </a>
+            </div>
+            <div class="cont07__gallery__item">
+                <a  href="https://www.youtube.com/embed/H5v3kku4y6Q" data-fancybox="galeria-video">
+                    <img src="{{asset('storage/uploads/tmp/logo-for.png')}}" />
+                </a>
+            </div>
+            <div class="cont07__gallery__item">
+                <a  href="https://www.youtube.com/embed/H5v3kku4y6Q" data-fancybox="galeria-video">
+                    <img src="{{asset('storage/uploads/tmp/logo-for.png')}}" />
+                </a>
+            </div>
+            <div class="cont07__gallery__item">
+                <a  href="https://www.youtube.com/embed/H5v3kku4y6Q" data-fancybox="galeria-video">
+                    <img src="{{asset('storage/uploads/tmp/logo-for.png')}}" />
+                </a>
+            </div>
         </div>
         <a href="#" class="cont07__cta transition d-flex justify-content-center align-items-center">
             <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="" class="cont07__cta__icon me-3 transition">
--- resources/views/Client/pages/Contents/CONT07/src/main.js
+++ resources/views/Client/pages/Contents/CONT07/src/main.js
@@ -1,21 +1,42 @@
-var valorAntigo = $("#videoApre").attr('data-src');
-var caminhoCapaVideo = $("#videoApre").attr('data-capa-video');
-var arrValorAntigo = new Array();
-if(valorAntigo.indexOf("/embed/") > 0){
-        if(valorAntigo != ""){
-            arrValorAntigo = valorAntigo.split("/embed/");
-            if(arrValorAntigo.length > 1){
-                    arrValorAntigo = arrValorAntigo[1].split('&');
-                    if(caminhoCapaVideo == ""){
-                            $("#videoApre").css('background-image', 'url(https://i.ytimg.com/vi/'+arrValorAntigo[0]+'/hqdefault.jpg)');
-                    }else{
-                            $("#videoApre").css('background-image', 'url('+caminhoCapaVideo+')');
+$(function(){
+        $('.carousel-gallery-cont07').owlCarousel({
+                loop:false,
+                autoplay:false,
+                nav:false,
+                dots: true,
+                margin:5,
+                stagePadding:0,
+                smartSpeed:450,
+                autoplayTimeout:5000,
+                rewind: true,
+                autoHeight: true,
+                responsive:{
+                    0:{
+                        items:2,
+                        margin:-25
+                    },
+                    500:{
+                        items:2,
+                        margin:-25
+                    },
+                    992:{
+                        items:3,
+                        margin:-25
+                    },
+                     800:{
+                        items:4
+                    },
+                    1200:{
+                        items:4
                     }
-            }
+                }
+        });
+        $('.carousel-gallery-cont07').css('width', $(window).outerWidth() - 213);
+        $(window).resize(function(){
+                $('.carousel-gallery-cont07').css('width', $(window).outerWidth());
+        });
+        if($(window).outerWidth() <= 600){
+                $('.carousel-gallery-cont07').css('width', $(window).outerWidth());
         }
-}
-$('body').on('click', '#videoApre .play', function(){
-    $urlVideo = $(this).parents('#videoApre').attr('data-src');
-    $(this).parents('.cont07__boxVideo__content').append('<iframe id="urlYoutube" width="99.5%" height=500" src="'+$urlVideo+'?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    $(this).parents('#videoApre').remove();
-}); 
+})
+    
