ee297b86576854d9cfb19924546fd3c61cdc7cbf
Anderson de Souza Viana <contato.nandos@hotmail.com>
2023-03-01 09:18:54 -0300

    Remove function deviceDetected in slide

--- resources/views/Admin/cruds/Slides/SLID01/form.blade.php
+++ resources/views/Admin/cruds/Slides/SLID01/form.blade.php
@@ -1,7 +1,7 @@
 <ul class="mb-0 nav nav-tabs" id="tooltip-container">
     <li class="nav-item">
         <a href="#slideDesktop" data-bs-toggle="tab" aria-expanded="true" class="nav-link d-flex align-items-center active">
-            Informações para Desktop {{ deviceDetect() }}
+            Informações para Desktop
             <i href="javascript:void(0)" class="mdi mdi-help-circle font-20 ms-2 btn-icon"
                 data-bs-container="#tooltip-container" data-bs-toggle="tooltip" data-bs-placement="top"
                 data-bs-original-title="Cadastrar informações para o slide que será exibido quando o site for aberto no desktop"></i>
