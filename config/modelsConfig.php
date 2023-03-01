a42b7f3e62e01ac0ef47bf72e8ba6d9873b97a17
mercesleonardo <leonardokarvalho@gmail.com>
2023-03-01 13:02:59 -0300

    Inicio backend XandAviao

--- config/modelsConfig.php
+++ config/modelsConfig.php
@@ -4,13 +4,36 @@ return [
     // Configure the model the header and footer, not change the module
     'InsertModelsCore' => (object)[
         'Headers' => (object)[
+            'Code' => 'HEAD02'
         ],
         'Footers' => (object)[
+            'Code' => 'FOOT04'
         ]
     ],
 
     // Configure existing modules and templates site-wide/system
     'InsertModelsMain' => (object) [
+        'Slides' => (object) [
+            'SLID02' => (object)[
+                'ViewHome' => true,
+                'ViewListMenu' => true,
+                'ViewListPanel' => true,
+                'ViewListFooter' => true,
+                'Viewer' => 'list',
+                'config' => (object) [
+                    'titleMenu' => 'Redes Sociais',
+                    'anchor' =>  true,
+                    'linkMenu' => '#SLID02',
+                    'iconMenu' => '',
+                    'titlePanel' => 'Redes Sociais',
+                    'iconPanel' => 'mdi-information'
+                ],
+                'IncludeSections' => (object) [
+                    'Module' => '',
+                    'Module' => '',
+                ]
+            ],
+        ],
     ],
 
     'ModelsForm' => (object)[
