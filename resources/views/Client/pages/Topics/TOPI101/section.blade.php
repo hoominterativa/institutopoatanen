39af369acf759b42ace1bfa2c07072b5de8f79a4
Rodrigo <rodrigoprofissional2014@gmail.com>
2023-03-08 16:05:58 -0300

    topi101 pattern setting

---
+++ resources/views/Client/pages/Topics/TOPI101/section.blade.php
@@ -0,0 +1,26 @@
+<section id="TOPI101" class="topi101 container-fluid">
+    <div class="container">
+        <div class="topi101__encompass">
+            <h4 class="topi101__encompass__title">Titulo</h4>
+            <h5 class="topi101__encompass__subtitle">Subtitulo</h5>
+        </div>
+        {{-- END topi101__encompass --}}
+        <div class="topi101__content carousel-topi101 owl-carousel">
+            <div class="topi101__box">
+                <div class="topi101__box__image">
+                    <img src="{{asset('storage/uploads/tmp/image-box.jpg')}}" loading="lazy" alt="">
+                </div>
+                <div class="topi101__box__description">
+                     <div class="topi101__box__description__paragraph">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {{-- END .topi101__box --}}
+        </div>
+        {{-- END .topi101__content --}}
+    </div>
+    {{-- END .container --}}
+</section>    
