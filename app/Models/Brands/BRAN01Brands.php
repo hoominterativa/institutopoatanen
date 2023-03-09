3da6285823b3b720b1b87dfe32812cc08b295e59
frontrgomes <rodrigoprofissiona2014@gmail.com>
2023-03-09 16:44:10 -0300

    ajusting module BRAIN01

---
+++ app/Models/Brands/BRAN01Brands.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models\Brands;
+
+use Database\Factories\Brands\BRAN01BrandsFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BRAN01Brands extends Model
+{
+    use HasFactory;
+
+    protected static function newFactory()
+    {
+        return BRAN01BrandsFactory::new();
+    }
+
+    protected $table = "";
+    protected $fillable = [];
+
+    public function scopeSorting($query)
+    {
+        return $query->orderBy('sorting', 'ASC');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('active', 1);
+    }
+
+    // public function getRelationCore()
+    // {
+    //     return null;
+    // }
+}
