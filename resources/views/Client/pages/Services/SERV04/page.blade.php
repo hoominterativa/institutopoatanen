d3075ba94df3f5ab08f5605609efc331833692eb
Rodrigo <rodrigoprofissional2014@gmail.com>
2023-03-10 17:00:44 -0300

    fix: ajusting css section SERV04

--- resources/views/Client/pages/Services/SERV04/page.blade.php
+++ resources/views/Client/pages/Services/SERV04/page.blade.php
@@ -1,75 +1,7 @@
 @extends('Client.Core.client')
 @section('content')
 {{-- BEGIN Page content --}}
-<section id="SERV04" class="serv04-page container-fluid px-0">
-    <header class="serv04-page__header" style="background-image: url({{asset('storage/uploads/tmp/bg-banner-inner.jpg')}})">
-        <h2 class="container container--serv04-page d-block text-center">
-            <span class="serv04-page__header__title d-block">Titulo</span>
-            <span class="serv04-page__header__subtitle d-block">Subtitulo</span>
-            <hr class="serv04-page__header__line mb-0">
-        </h2>
-    </header>
-    <main class="serv04-page__main">
-        <div class="container container--serv04-page__main">
-            <div class="serv04-page__encompass px-0 text-center mx-auto w-100">
-                <h2 class="serv04-page__encompass__title">Titulo</h2>
-                <h3 class="serv04-page__encompass__subtitle">subtitulo</h3>
-                <hr class="serv04-page__encompass__line">
-                <div class="serv04-page__encompass__paragraph mx-auto">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor
-                        eu purus gravida sollicitudin vel non libero. Vivamus commodo porta 
-                        velit, vel tempus mi pretium sed. In et arcu eget purus mattis posuere. Donec tincidunt dignissim faucibus. 
-                    </p>
-                </div>
-            </div>
-            <div class="serv04-page__content">
-                <div class="row row--serv04-page w-100 mx-auto">
-                    <div class="serv04-page__box col-sm-3">
-                        <div class="serv04-page__box__content">
-                            <div class="serv04-page__box__image">
-                                <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                        </div>
-                        </div>
-                    </div>
-                    {{-- END .serv04-page__box --}}
-                    <div class="serv04-page__box col-sm-3">
-                        <div class="serv04-page__box__content">
-                            <div class="serv04-page__box__image">
-                                <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                        </div>
-                        </div>
-                    </div>
-                    {{-- END .serv04-page__box --}}
-                    <div class="serv04-page__box col-sm-3">
-                        <div class="serv04-page__box__content">
-                            <div class="serv04-page__box__image">
-                                <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                        </div>
-                        </div>
-                    </div>
-                    {{-- END .serv04-page__box --}}
-                    <div class="serv04-page__box col-sm-3">
-                        <div class="serv04-page__box__content">
-                            <div class="serv04-page__box__image">
-                                <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                        </div>
-                        </div>
-                    </div>
-                    {{-- END .serv04-page__box --}}
-                    <div class="serv04-page__box col-sm-3">
-                        <div class="serv04-page__box__content">
-                            <div class="serv04-page__box__image">
-                                <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                        </div>
-                        </div>
-                    </div>
-                    {{-- END .serv04-page__box --}}
-                </div>
-            </div>
-        </div>
-    </div>
-</section>    
+  
 {{-- Finish Content page Here --}}
 @foreach ($sections as $section)
     {!!$section!!}
--- resources/views/Client/pages/Services/SERV04/section.blade.php
+++ resources/views/Client/pages/Services/SERV04/section.blade.php
@@ -1,64 +1,51 @@
 <section id="SERV04" class="serv04 container-fluid" style="background-image: url({{asset('storage/uploads/tmp/image-box-white.jpg')}})">
-    <div class="container container--serv04">
-        <div class="row">
-            <div class="serv04__encompass px-0 text-center mx-auto w-100">
-                <h2 class="serv04__encompass__title">Titulo</h2>
-                <h3 class="serv04__encompass__subtitle">subtitulo</h3>
-                <hr class="serv04__encompass__line">
-                <div class="serv04__encompass__paragraph mx-auto">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor
-                        eu purus gravida sollicitudin vel non libero. Vivamus commodo porta 
-                        velit, vel tempus mi pretium sed. In et arcu eget purus mattis posuere. Donec tincidunt dignissim faucibus. 
-                    </p>
-                </div>
-            </div>
-            {{-- END .serv04__encompass --}}
-            <div class="serv04__content row mx-auto px-0">
-                <div class="serv04__box col-sm-3">
-                    <div class="serv04__box__content">
-                        <div class="serv04__box__image">
-                            <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                       </div>
-                    </div>
-                </div>
-                {{-- END .serv04__box --}}
-                <div class="serv04__box col-sm-3">
-                    <div class="serv04__box__content">
-                        <div class="serv04__box__image">
-                            <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                       </div>
-                    </div>
-                </div>
-                {{-- END .serv04__box --}}
-                <div class="serv04__box col-sm-3">
-                    <div class="serv04__box__content">
-                        <div class="serv04__box__image">
-                            <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                       </div>
-                    </div>
-                </div>
-                {{-- END .serv04__box --}}
-                <div class="serv04__box col-sm-3">
-                    <div class="serv04__box__content">
-                        <div class="serv04__box__image">
-                            <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                       </div>
-                    </div>
-                </div>
-                {{-- END .serv04__box --}}
-                <div class="serv04__box col-sm-3">
-                    <div class="serv04__box__content">
-                        <div class="serv04__box__image">
-                            <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
-                       </div>
-                    </div>
-                </div>
-                {{-- END .serv04__box --}}
-            </div>
-            {{-- END .serv04__content --}}
+    <header class="serv04__header" >
+        <h2 class="container container--serv04 d-block text-center">
+            <span class="serv04__header__title d-block">Titulo</span>
+            <span class="serv04__header__subtitle d-block">Subtitulo</span>
+            <hr class="serv04__header__line mb-0">
+        </h2>
+        <div class="serv04__header__paragraph mx-auto text-center">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor
+                eu purus gravida sollicitudin vel non libero. Vivamus commodo porta 
+                velit, vel tempus mi pretium sed. In et arcu eget purus mattis posuere. Donec tincidunt dignissim faucibus. 
+            </p>
         </div>
-    </div>
-    {{-- END .serv04__container --}}
-</section>
-{{-- END .serv04 --}}
+    </header>   
+    <main class="serv04__content">
+        <div class="container">
+            <div class="row row--serv04 w-100 mx-auto">
+                <div class="serv04__box col-sm-3">
+                    <div class="serv04__box__content">
+                        <div class="serv04__box__bg">
+                            <img src="{{asset('storage/uploads/tmp/image-box.jpg')}}" alt="Logo" loading="lazy">
+                        </div>
+                        <div class="serv04__box__description">
+                            <div class="serv04__box__image">
+                                <img src="{{asset('storage/uploads/tmp/logo.svg')}}" alt="Logo" loading="lazy">
+                            </div>
+                            <h4 class="serv04__box__title">Titulo Topico</h4>
+                            <div class="serv04__box__paragraph">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor
+                                    eu purus gravida sollicitudin vel non libero. Vivamus commodo porta 
+                                    velit, vel tempus mi pretium sed. In et arcu eget purus mattis posuere. Donec tincidunt dignissim faucibus. 
+                                </p>
+                            </div>
+                            <a rel="first" href="sobre" class="serv04__box__cta transition justify-content-center align-items-center">
+                                <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Icone CTA" class="serv04__box__cta__icon me-3 transition">
+                                CTA
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                {{-- END .serv04__box --}}
+            </div>
+            <a rel="first" href="sobre" class="serv04__ctaPrincipal transition justify-content-center align-items-center">
+                <img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" alt="Icone CTA" class="serv04__ctaPrincipal__icon me-3 transition">
+                CTA
+            </a>
+        </div>
+    </main>
+</section>  
