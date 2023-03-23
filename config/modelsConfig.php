0c9c5fa43fa7f466a203d41dcf901ee7ed1c2768
Anderson de Souza Viana <contato.nandos@hotmail.com>
2023-03-23 14:50:31 -0300

    Cleared code in modelsConfig

--- config/modelsConfig.php
+++ config/modelsConfig.php
@@ -3,56 +3,18 @@
 return [
     // Configure the model the header and footer, not change the module
     'InsertModelsCore' => (object)[
-        'Headers' => (object)[
-            'Code' => 'HEAD02',
-            'themeMenu' => 'SIDE02'
-        ],
-        'Footers' => (object)[
-            'Code' => 'FOOT03'
-        ]
+        'Headers' => (object)[],
+        'Footers' => (object)[]
     ],
 
     // Configure existing modules and templates site-wide/system
     'InsertModelsMain' => (object) [
-        'Blogs' => (object) [
-            'BLOG01' => (object)[
-                'ViewHome' => true,
-                'ViewListMenu' => false,
-                'ViewListPanel' => true,
-                'config' => (object) [
-                    'titleMenu' => 'Blogs',
-                    'anchor' =>  false,
-                    'linkMenu' => 'blog01.page',
-                    'iconMenu' => '',
-                    'titlePanel' => 'Artigos',
-                    'iconPanel' => 'mdi-information'
-                ],
-                'IncludeSections' => (object) []
-            ],
-        ],
-        'Services' => (object) [
-            'SERV04' => (object)[
-                'ViewHome' => true,
-                'ViewListMenu' => false,
-                'ViewListPanel' => true,
-                'config' => (object) [
-                    'titleMenu' => 'Serviços',
-                    'anchor' =>  false,
-                    'linkMenu' => '',
-                    'iconMenu' => '',
-                    'titlePanel' => 'Serviços',
-                    'iconPanel' => 'mdi-information'
-                ],
-                'IncludeSections' => (object) []
-            ],
-        ],
+
     ],
 
     'ModelsForm' => (object)[
         'FORM101' => 'Contacts_FORM101.jpg',
     ],
 
-    'ModelsCompliances' => (object)[
-        'Code' => 'COMP01'
-    ],
+    'ModelsCompliances' => (object)[],
 ];
