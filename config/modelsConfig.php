abf2c453bc34d5286a481663ad146a3b3e2771bf
Anderson de Souza Viana <contato.nandos@hotmail.com>
2023-04-20 09:25:22 -0300

    Update modelsConfig.php

--- config/modelsConfig.php
+++ config/modelsConfig.php
@@ -4,48 +4,14 @@ return [
     // Configure the model the header and footer, not change the module
     'InsertModelsCore' => (object)[
         'Headers' => (object)[
-            'Code' => 'HEAD02',
-            'themeMenu' => 'SIDE02'
         ],
         'Footers' => (object)[
-            'Code' => 'FOOT02'
         ]
     ],
 
     // Configure existing modules and templates site-wide/system
     'InsertModelsMain' => (object) [
-        'Portfolios' => (object) [
-            'PORT02' => (object)[
-                'ViewHome' => true,
-                'ViewListMenu' => true,
-                'ViewListPanel' => true,
-                'ViewListFooter' => false,
-                'Viewer' => 'dropdown', // accepted values, list or dropdown
-                'IncludeCore' => (object) [
-                    'include' => true,
-                    'sorting' => true,
-                    'limit' => 'all',
-                    'condition' => 'active=1{Ativos},featured=1{Detaques Home}',
-                    'titleList' => 'title',
-                    'relation' => (object)[
-                        'category' =>(object)[
-                            'name' => 'Categoria',
-                            'titleList' => 'title',
-                            'condition' => 'active=1{Ativos},featured=1{Detaques Home}',
-                        ]
-                    ],
-                ],
-                'config' => (object) [
-                    'titleMenu' => 'Portifólio',
-                    'anchor' =>  false,
-                    'linkMenu' => 'pot02.page',
-                    'iconMenu' => '',
-                    'titlePanel' => 'Portifólio',
-                    'iconPanel' => ''
-                ],
-                'IncludeSections' => (object) []
-            ],
-        ],
+
     ],
 
     'ModelsForm' => (object)[
