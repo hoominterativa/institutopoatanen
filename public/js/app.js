8df1bd918345aa22d3dd6f30eee25047abcfefe2
frontrgomes <70538582+rgomes2014@users.noreply.github.com>
2023-05-08 08:44:25 -0300

    finalização do css section

--- public/js/app.js
+++ public/js/app.js
@@ -12113,9 +12113,47 @@ $(function () {
 /*!***************************************************************!*\
   !*** ./resources/views/Client/pages/Blogs/BLOG03/src/main.js ***!
   \***************************************************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
+/* provided dependency */ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+$(function () {
+  $('.blog03__boxs__carousel').owlCarousel({
+    smartSpeed: 450,
+    loop: false,
+    dots: true,
+    nav: false,
+    rewind: true,
+    autoHeight: true,
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+        margin: -100
+      },
+      // breakpoint from 0
+      200: {
+        items: 1,
+        margin: -100
+      },
+      // breakpoint from 200 up
+      500: {
+        items: 1,
+        margin: -100
+      },
+      // breakpoint from 815 up
+      820: {
+        items: 1,
+        margin: -50
+      } // breakpoint from 820 up
 
+    }
+  });
+  $('.blog03__boxs__carousel').css('width', $(window).outerWidth() + 150);
+
+  if ($(window).outerWidth() <= 801) {
+    $('.blog03__boxs__carousel').css('width', $(window).outerWidth());
+  }
+});
 
 /***/ }),
 
--- resources/views/Client/pages/Blogs/BLOG03/section.blade.php
+++ resources/views/Client/pages/Blogs/BLOG03/section.blade.php
@@ -8,11 +8,11 @@
             <hr class="blog03__header__line">
             <p class="blog03__header__paragraph">dsadsadsad</p>
         </header>
-        <div class="blog03__boxs row blog03__boxs__carousel">
+        <div class="blog03__boxs row blog03__boxs__carousel owl-carousel">
             <article class="blog03__boxs__item">
                 <a itemprop="url" href="#" class="link-full"></a>
                 <div class="blog03__boxs__item__content d-flex justify-content-between row transition">
-                    
+
                     <figure class="blog03__boxs__item__image px-0">
                         <img itemprop="image" src="{{asset('storage/uploads/tmp/inner-image.jpg')}}" class="blog03__boxs__item__image__img" width="100%" alt="image"/>
                     </figure>
@@ -20,7 +20,7 @@
                         <h2 itemprop="name" class="blog03__boxs__item__title"><img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" width="34" class="blog03__boxs__item__title__icon" alt="title"/> Titulo Topico</h2>
                         <div class="blog03__boxs__item__paragraph">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu eget purus mattis posuere. Donec tincidunt dignissim faucibus. 
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu eget purus mattis posuere. Donec tincidunt dignissim faucibus.
                             </p>
                         </div>
                         <div class="blog03__boxs__item__cta">
@@ -29,6 +29,28 @@
                     </div>
                 </div>
             </article>
+            {{-- Fim-blog03__boxs__item --}}
+            <article class="blog03__boxs__item">
+                <a itemprop="url" href="#" class="link-full"></a>
+                <div class="blog03__boxs__item__content d-flex justify-content-between row transition">
+
+                    <figure class="blog03__boxs__item__image px-0">
+                        <img itemprop="image" src="{{asset('storage/uploads/tmp/inner-image.jpg')}}" class="blog03__boxs__item__image__img" width="100%" alt="image"/>
+                    </figure>
+                    <div class="blog03__boxs__item__description col">
+                        <h2 itemprop="name" class="blog03__boxs__item__title"><img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" width="34" class="blog03__boxs__item__title__icon" alt="title"/> Titulo Topico</h2>
+                        <div class="blog03__boxs__item__paragraph">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu eget purus mattis posuere. Donec tincidunt dignissim faucibus.
+                            </p>
+                        </div>
+                        <div class="blog03__boxs__item__cta">
+                            <a href="#"><img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" class="blog03__boxs__item__cta__icon" alt="title"/> CTA</a>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            {{-- Fim-blog03__boxs__item --}}
         </div>
         <div class="blog03__cta">
             <a href="#"><img src="{{asset('storage/uploads/tmp/icon-general.svg')}}" class="blog03__cta__icon" alt="title"/> CTA</a>
