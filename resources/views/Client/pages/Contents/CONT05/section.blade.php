80a95020c07f314e8c48d57c1137dc98519f1085
frontrgomes <70538582+rgomes2014@users.noreply.github.com>
2023-05-23 23:12:44 -0300

    configuração cont05

---
+++ resources/views/Client/pages/Contents/CONT05/section.blade.php
@@ -0,0 +1,21 @@
+
+        <section id="CONT05" class="cont05 container-fluid px-0" style="background-image: url({{ asset('storage/' . $content->path_image_desktop) }}); background-color: {{ $content->background_color }};">
+            <div class="d-flex justify-content-center align-content-center flex-column">
+                <header class="cont05__header">
+                        <h3 class="cont05__header__title">
+                            title
+                        </h3>
+                        <hr class="cont05__header__line">
+                        <p class="cont05__header__paragraph">asdasdasdasdasdasd</p>
+                </header>
+                <div class="cont05__content">
+                    <a href="" target=""
+                        class="cont05__content__cta transition d-flex justify-content-center align-items-center">
+                        <img src="" alt="" class="cont05__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </section>
+
+{{-- END #CONT05 --}}
