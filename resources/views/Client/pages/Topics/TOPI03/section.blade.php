5e097e8dfdadc8b7f386945c7c1b4ce1ce996bb5
frontrgomes <70538582+rgomes2014@users.noreply.github.com>
2023-05-23 23:00:24 -0300

    Configuração de modulos

---
+++ resources/views/Client/pages/Topics/TOPI03/section.blade.php
@@ -0,0 +1,32 @@
+
+        <section id="TOPI03" class="container-fluid" style="background-image: url(); background-color: ;">
+            <div class="container">
+                    <header class="header-topic">
+                        <h3 class="container-title">
+                                <span class="title">title</span>
+                                <span class="subtitle">subtitle</span>
+                        </h3>
+                        <hr class="line">
+                        <p class="paragraph">subtitle</p>
+                    </header>
+                <div class="container-box row">
+                        <article class="box-topic col-sm-4">
+                            <div class="content transition">
+                                <a href="#" target="_blank">
+                                    <img src="" width="100%" height="100%" class="position-absolute top-0 start-0" alt="">
+                                    <div class="container-info d-flex flex-column justify-content-center align-items-center">
+                                        <figure class="image">
+                                            <img src="" class="icon" width="61" alt="">
+                                        </figure>
+                                        <div class="description">
+                                            <h3 class="title">title</h3>
+                                            <p class="paragraph">paragraph</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </article>
+                    {{-- END .box-topic --}}
+                </div>
+            </div>
+        </section>
