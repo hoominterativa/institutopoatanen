dd68b7750ef6a30bcfe74cc44102bf05314f29ad
Rodrigo <rodrigoprofissional2014@gmail.com>
2023-05-31 10:09:42 -0300

    retirada de model

--- config/modelsConfig.php
+++ config/modelsConfig.php
@@ -257,40 +257,6 @@ return [
                 ],
             ],
         ],
-        'Products.2' => (object) [
-            'PROD02V1' => (object)[
-                'ViewHome' => true,
-                'ViewListMenu' => true,
-                'ViewListPanel' => true,
-                'ViewListFooter' => true,
-                'Viewer' => 'dropdown', // accepted values, list or dropdown
-                'IncludeCore' => (object) [
-                    'include' => true,
-                    'sorting' => true,
-                    'limit' => 'all',
-                    'condition' => 'active=1{Ativos},featured=1{Detaques}',
-                    'titleList' => 'title',
-                    'relation' => (object)[
-                        'category' =>(object)[
-                            'name' => 'Categoria',
-                            'titleList' => 'title',
-                            'condition' => 'active=1{Ativos}',
-                        ]
-                    ],
-                ],
-                'config' => (object) [
-                    'titleMenu' => 'Procedimentos',
-                    'anchor' =>  false,
-                    'linkMenu' => 'prod02v1.page',
-                    'iconMenu' => '',
-                    'titlePanel' => 'Procedimentos',
-                    'iconPanel' => 'mdi-shopping-outline'
-                ],
-                'IncludeSections' => (object) [
-                    'Topics' => 'TOPI02',
-                ]
-            ],
-        ],
         'Frequently' => (object) [
             'FREQ01' => (object)[
                 'ViewHome' => false,
