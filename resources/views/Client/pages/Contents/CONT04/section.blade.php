b6484caca0425720b48ae04e8cd4277b74ce6d06
ia5go <benvindoiago@outlook.com>
2023-06-05 15:02:56 -0300

    CONT04

---
+++ resources/views/Client/pages/Contents/CONT04/section.blade.php
@@ -0,0 +1,46 @@
+<section id="CONT04" class="cont04 container-fluid"
+    style="background-image: url({{ asset('storage/uploads/tmp/bg-slide.jpg') }}); ">
+    <div class="container">
+
+        <header class="cont04__header w-100 d-flex flex-column align-items-center">
+            <h2 class="cont04__header__title text-center">Título</h2>
+
+            <h3 class="cont04__header__subtitle text-center">Subtítulo</h3>
+
+            <hr class="cont04__header__line">
+
+            <div class="cont04__header__desc text-center">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida
+                    sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu eget
+                    purus mattis posuere. Donec tincidunt dignissim faucibus.
+                </p>
+            </div>
+        </header>
+
+        <main class="cont04__main w-100">
+            <img src="{{ asset('storage/uploads/tmp/png-slide.png') }}" alt="" class="cont04__main__image">
+
+            <div class="cont04__main__content d-flex flex-column align-self-stretch">
+                <h4 class="cont04__main__subtitle">Subtítulo</h4>
+
+                <h4 class="cont04__main__title">Título</h4>
+
+                <hr class="cont04__main__line">
+
+                <div class="cont04__main__desc w-100">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida
+                        sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu
+                        get purus mattis posuere. Donec tincidunt dignissim faucibus. </p>
+                </div>
+
+                <a href="" class="cont04__main__cta">
+                    <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}"
+                        class="cont04__main__content__cta__icon" alt="Ícone do botão">
+                    CTA
+                </a>
+            </div>
+
+        </main>
+
+    </div>
+</section>
