24be0103e05cd481a74f32cb768ee4876d76fc75
ia5go <benvindoiago@outlook.com>
2023-06-06 17:57:51 -0300

    ABOU04
    Seção da home finalizada;
    banner e conteudo na pg feitos;

---
+++ resources/views/Client/pages/Abouts/ABOU04/section.blade.php
@@ -0,0 +1,27 @@
+<section class="abou04 w-100" id="abou04"
+    style="background-image: url({{ asset('storage/uploads/tmp/bg-section-gray.jpg') }});">
+
+    <main class="abou04__main container">
+
+        <img src="{{ asset('storage/uploads/tmp/image-pmg.png') }}" alt="" class="abou04__image">
+
+        <div class="abou04__content d-flex flex-column align-items-start">
+            <h2 class="abou04__title">Título</h2>
+            <h3 class="abou04__subtitle">Subtítulo</h3>
+            <hr class="abou04__line">
+
+            <div class="abou04__desc">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida
+                    sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu eget
+                    purus mattis posuere. Donec tincidunt dignissim faucibus.</p>
+            </div>
+
+            <a href="#" class="abou04__cta">
+                <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="abou04__cta__icon">
+                CTA
+            </a>
+        </div>
+
+    </main>
+
+</section>
