66d89754612bcec76fb9ecb25258ae399786fafd
frontrgomes <70538582+rgomes2014@users.noreply.github.com>
2023-06-13 09:34:11 -0300

    ajuste modulo

--- resources/views/Client/assets/js/base.js
+++ resources/views/Client/assets/js/base.js
@@ -1,5 +1,12 @@
 import '../../Core/Headers/HEAD02/src/main';
 import '../../Components/themeMenu/SIDE02/src/main';
 import '../../Core/Footers/FOOT02/src/main';
-import '../../pages/Abouts/ABOU04/src/main';
+import '../../pages/Slides/SLID01/src/main';
+import '../../pages/Abouts/ABOU01/src/main';
+import '../../pages/Services/SERV04/src/main';
+import '../../pages/Contents/CONT02/src/main';
+import '../../pages/Brands/BRAN01/src/main';
+import '../../pages/Blogs/BLOG01/src/main';
+import '../../pages/Feedbacks/FEED01/src/main';
+import '../../pages/Topics/TOPI101/src/main';
 import '../../pages/Compliances/COMP01/src/main';
