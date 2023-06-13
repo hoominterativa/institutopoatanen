8d900543164e8e1533a5b38c97a62a84df211115
frontrgomes <70538582+rgomes2014@users.noreply.github.com>
2023-06-13 15:07:54 -0300

    ajuste estrutura cota

--- resources/views/Client/pages/Contacts/COTA02/page.blade.php
+++ resources/views/Client/pages/Contacts/COTA02/page.blade.php
@@ -2,41 +2,41 @@
 @section('content')
     {{-- BEGIN Page content --}}
     @if ($contact)
-        <section id="COTA02" class="cota02-page">
+        <section id="CONT02" class="cont02-page">
             <div class="container-fluid px-0">
-                <header class="cota02-page__header"
+                <header class="cont02-page__header"
                     style="background-image: url({{ asset('storage/' . $contact->path_image_banner_desktop) }}); background-color: {{ $contact->background_color_banner }};">
                     @if ($contact->path_image_banner_desktop)
-                        <div class="cota02-page__header__mask"></div>
+                        <div class="cont02-page__header__mask"></div>
                     @endif
-                    {{-- <div class="cota02-page__header__mask"></div> --}}
-                    <div class="container container-cota02-page__header">
+                    {{-- <div class="cont02-page__header__mask"></div> --}}
+                    <div class="container container-cont02-page__header">
                         @if ($contact->title_banner || $contact->subtitle_banner)
-                            <h2 class="cota02-page__header__title d-block">{{ $contact->title_banner }}</h2>
-                            <h3 class="cota02-page__header__subtitle d-block">{{ $contact->subtitle_banner }}
+                            <h2 class="cont02-page__header__title d-block">{{ $contact->title_banner }}</h2>
+                            <h3 class="cont02-page__header__subtitle d-block">{{ $contact->subtitle_banner }}
                             </h3>
                         @endif
-                        <hr class="cota02-page__header__line mb-0">
+                        <hr class="cont02-page__header__line mb-0">
                     </div>
                 </header>
-                <div class="cota02-page__boxForm"
+                <div class="cont02-page__boxForm"
                     style="background-image: url({{ asset('storage/' . $contact->path_image_topic_desktop) }}); background-color: {{ $contact->background_color_topic }};">
                     <div class="container container--boxForm">
                         <div class="row justify-content-center">
                             @foreach ($topics as $topic)
-                                <div class="cota02-page__boxForm__item col-sm-4">
-                                    <div class="cota02-page__boxForm__item__content">
-                                        <div class="cota02-page__boxForm__item__content__image mx-auto">
+                                <div class="cont02-page__boxForm__item col-sm-4">
+                                    <div class="cont02-page__boxForm__item__content">
+                                        <div class="cont02-page__boxForm__item__content__image mx-auto">
                                             <img src="{{ asset('storage/' . $topic->path_image_icon) }}" class="w-100 h-100"
                                                 alt="Imagem Perfil">
                                         </div>
-                                        <div class="cota02-page__boxForm__item__content__description text-center">
+                                        <div class="cont02-page__boxForm__item__content__description text-center">
                                             @if ($topic->title)
-                                                <h2 class="cota02-page__boxForm__item__content__description__title">
+                                                <h2 class="cont02-page__boxForm__item__content__description__title">
                                                     {{ $topic->title }}</h2>
                                             @endif
                                             @if ($topic->description)
-                                                <div class="cota02-page__boxForm__item__content__description__paragraph">
+                                                <div class="cont02-page__boxForm__item__content__description__paragraph">
                                                     <p>
                                                         {!! $topic->description !!}
                                                     </p>
@@ -49,31 +49,31 @@
                         </div>
                     </div>
                 </div>
-                <div class="cota02-page__form"
+                <div class="cont02-page__form"
                     style="background-image: url({{ asset('storage/' . $contact->path_image_form_desktop) }}); background-color: {{ $contact->background_color_form }};">
                     <div class="container">
-                        <div class="cota02-page__form__header">
+                        <div class="cont02-page__form__header">
                             @if ($contact->title_form)
-                                <h2 class="cota02-page__form__header__title">{{ $contact->title_form }}</h2>
+                                <h2 class="cont02-page__form__header__title">{{ $contact->title_form }}</h2>
                             @endif
-                            {{-- <h4 class="cota02-page__form__header__line"></h4> --}}
+                            {{-- <h4 class="cont02-page__form__header__line"></h4> --}}
                             @if ($contact->description_form)
-                                <div class="cota02-page__form__header__paragraph">
+                                <div class="cont02-page__form__header__paragraph">
                                     <p>
                                         {!! $contact->description_form !!}
                                     </p>
                                 </div>
                             @endif
                         </div>
-                        <div class="cota02-page__form__inputs">
+                        <div class="cont02-page__form__inputs">
                             <form action=""
-                                class="cota02-page__form__inputs__formIput parsley-validate flex-column mx-auto">
+                                class="cont02-page__form__inputs__formIput parsley-validate flex-column mx-auto">
                                 <input type="hidden" name="target_lead" value="{{ $contact->title_page }}">
                                 <input type="hidden" name="target_send" value="{{ base64_encode($contact->email_form) }}">
                                 <div class="row">
                                     @foreach ($inputs as $name => $input)
                                         <div
-                                            class="cota02-show__form__item__input col-12 {{ $input->type != 'textarea' ? 'col-sm-6' : '' }}">
+                                            class="cota01-show__form__item__input col-12 {{ $input->type != 'textarea' ? 'col-sm-6' : '' }}">
                                             @include('Client.Components.inputs', [
                                                 'name' => $name,
                                                 'options' => $input->option,
@@ -84,8 +84,8 @@
                                         </div>
                                     @endforeach
                                 </div>
-                                <div class="cota02-show__form__footer d-flex align-items-center">
-                                    <div class="cota02-show__form__compliance form-check d-flex align-items-center">
+                                <div class="cota01-show__form__footer d-flex align-items-center">
+                                    <div class="cota01-show__form__compliance form-check d-flex align-items-center">
                                         {!! Form::checkbox('term_accept', 1, null, [
                                             'class' => 'form-check-input me-1',
                                             'id' => 'term_accept',
@@ -93,10 +93,10 @@
                                         ]) !!}
                                         {!! Form::label('term_accept', 'Aceito os termos descritos na ', ['class' => 'form-check-label']) !!}
                                         <a href="{{ getUri($compliance->link ?? '#') }}" target="_blank"
-                                            class="cota02-show__form__compliance__link ms-1">Política de Privacidade</a>
+                                            class="cota01-show__form__compliance__link ms-1">Política de Privacidade</a>
                                     </div>
                                     <button type="submit"
-                                        class="cota02-show__form__inputs__formIput__input-submit ms-auto">
+                                        class="cota01-show__form__inputs__formIput__input-submit ms-auto">
                                         {{ $contact->title_button_form }}
                                     </button>
                                 </div>
