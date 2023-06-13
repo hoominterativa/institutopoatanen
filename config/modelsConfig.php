fbde26442fa78403430bc0b8917ff4dd0c003e18
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2023-06-13 10:19:12 -0300

    Ajustes no modelsConfig

--- config/modelsConfig.php
+++ config/modelsConfig.php
@@ -60,8 +60,9 @@ return [
         'FORM03' => (object) [
             'model' => 'FORM03.jpg',
             'config' => (object) [
-                'title' => (object) ['name' => 'Título Seção', 'type' => 'text'],
-                'description' => (object) ['name' => 'Descrição Seção',  'type' => 'textarea'],
+                'title' => (object) ['name' => 'Título', 'type' => 'text'],
+                'subtitle' => (object) ['name' => 'Subtítulo',  'type' => 'text'],
+                'description' => (object) ['name' => 'Descrição',  'type' => 'textarea'],
                 'title_inner' => (object) ['name' => 'Título Lightbox',  'type' => 'text'],
                 'description_inner' => (object) ['name' => 'Descrição Lightbox',  'type' => 'textarea'],
                 'path_image_inner' => (object) ['name' => 'Imagem Lightbox',  'type' => 'image'],
