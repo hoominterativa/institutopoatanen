8f94fdeaf6f60dbfe10ee8a69c001ce69f443737
ia5go <benvindoiago@outlook.com>
2023-06-15 09:44:25 -0300

    Modelo finalizado

--- public/js/app.js
+++ public/js/app.js
@@ -12081,8 +12081,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Components_themeMenu_SIDE02_src_main__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_Components_themeMenu_SIDE02_src_main__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _Core_Footers_FOOT02_src_main__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../Core/Footers/FOOT02/src/main */ "./resources/views/Client/Core/Footers/FOOT02/src/main.js");
 /* harmony import */ var _Core_Footers_FOOT02_src_main__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_Core_Footers_FOOT02_src_main__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _pages_Feedbacks_FEED06_src_main__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../pages/Feedbacks/FEED06/src/main */ "./resources/views/Client/pages/Feedbacks/FEED06/src/main.js");
-/* harmony import */ var _pages_Feedbacks_FEED06_src_main__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_pages_Feedbacks_FEED06_src_main__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _pages_Contents_CONT11_src_main__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../pages/Contents/CONT11/src/main */ "./resources/views/Client/pages/Contents/CONT11/src/main.js");
+/* harmony import */ var _pages_Contents_CONT11_src_main__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_pages_Contents_CONT11_src_main__WEBPACK_IMPORTED_MODULE_3__);
 /* harmony import */ var _pages_Compliances_COMP01_src_main__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../pages/Compliances/COMP01/src/main */ "./resources/views/Client/pages/Compliances/COMP01/src/main.js");
 /* harmony import */ var _pages_Compliances_COMP01_src_main__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_pages_Compliances_COMP01_src_main__WEBPACK_IMPORTED_MODULE_4__);
 
@@ -12320,42 +12320,21 @@ $(function () {
 
 /***/ }),
 
-/***/ "./resources/views/Client/pages/Feedbacks/FEED06/src/main.js":
-/*!*******************************************************************!*\
-  !*** ./resources/views/Client/pages/Feedbacks/FEED06/src/main.js ***!
-  \*******************************************************************/
+/***/ "./resources/views/Client/pages/Contents/CONT11/src/main.js":
+/*!******************************************************************!*\
+  !*** ./resources/views/Client/pages/Contents/CONT11/src/main.js ***!
+  \******************************************************************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 /* provided dependency */ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
-$(".feed06__carousel").owlCarousel({
+$(".cont11__carousel").owlCarousel({
   smartSpeed: 450,
   loop: true,
-  dots: true,
-  nav: false,
+  dots: false,
+  nav: true,
   rewind: true,
   autoHeight: true,
-  margin: -60,
-  responsive: {
-    0: {
-      items: 1,
-      margin: 17
-    },
-    // breakpoint from 0 up
-    400: {
-      items: 1,
-      margin: 17
-    },
-    // breakpoint from 361 up
-    768: {
-      items: 1,
-      margin: 17
-    },
-    // breakpoint from 500 up
-    801: {
-      items: 2
-    } // breakpoint from 801 up
-
-  }
+  items: 1
 });
 
 /***/ }),
