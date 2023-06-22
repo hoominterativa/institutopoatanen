99a9fb2dee2798ae16882893a0514527c12c5db8
frontrgomes <70538582+rgomes2014@users.noreply.github.com>
2023-06-22 15:37:55 -0300

    ajuste css e html foot03

--- resources/views/Client/Core/Footers/FOOT03/app.blade.php
+++ resources/views/Client/Core/Footers/FOOT03/app.blade.php
@@ -6,28 +6,31 @@
         <div class="row align-items-center">
             <div class="foot03__top px-0">
                 <div class="d-flex align-items-center justify-content-between">
-                    <div class="foot03__top__telefone px-0">
-                        @if ($generalSetting->phone)
-                            <a href="Telefone:{{$generalSetting->phone}}" rel="next">
-                                <img src="{{asset('storage/uploads/tmp/logo-two.png')}}" alt="#">
-                                {{$generalSetting->phone}}
-                            </a>
-                        @endif
+                    <div class="content-nv">
+                        <div class="foot03__top__telefone px-0">
+                            @if ($generalSetting->phone)
+                                <a href="tel:{{$generalSetting->phone}}" rel="next">
+                                    <img src="{{asset('storage/uploads/tmp/icon-gray.png')}}" alt="#">
+                                    {{$generalSetting->phone}}
+                                </a>
+                            @endif
+                        </div>
+                        <div class="foot03__top__telefone px-0">
+                            @if ($generalSetting->whatsapp)
+                                <a href="https://api.whatsapp.com/send?phone=55{{Str::slug($generalSetting->whatsapp,'')}}" rel="next">
+                                    <img src="{{asset('storage/uploads/tmp/icon-gray.png')}}" alt="#">
+                                    {{$generalSetting->whatsapp}}
+                                </a>
+                            @endif
+                        </div>
                     </div>
-                    <span></span>
-                    <div class="foot03__top__telefone px-0">
-                        @if ($generalSetting->whatsapp)
-                            <a href="https://api.whatsapp.com/send?phone=55{{Str::slug($generalSetting->whatsapp,'')}}" rel="next">
-                                <img src="{{asset('storage/uploads/tmp/logo-two.png')}}" alt="#">
-                                {{$generalSetting->whatsapp}}
-                            </a>
-                        @endif
-                    </div>
-                    <div class="foot03__top__logo mx-auto">
+                    
+                    <div class="foot03__top__logo">
                         <a href="{{route('home')}}" rel="next">
                             <img src="{{asset('storage/'.$generalSetting->path_logo_footer_light??$generalSetting->path_logo_footer_dark)}}" class="w-100 h-100" alt="{{env('APP_NAME')}}">
                         </a>
                     </div>
+
                     <div class="foot03__top__redeSocial d-flex justify-content-between align-items-center">
                         @foreach ($socials as $social)
                             <a href="{{$social->link}}" class="social-link transition" title="{{$social->title}}">
@@ -38,9 +41,9 @@
                     </div>
                 </div>
             </div>
-            <div id="teste"foot03__bottom d-flex px-0">
-                <div class=" d-flex justify-content-between  col">
-                    <div class="foot03__bottom__boxLeft col px-0 d-flex">
+            <div id="teste" class="foot03__bottom d-flex px-0">
+                <div class=" d-flex justify-content-between  col footer-divider">
+                    <div class="foot03__bottom__boxLeft col-sm-6 px-0 d-flex">
                         <nav class="foot03__bottom__boxLeft__nav col px-0 mx-0">
                             <h4 class="foot03__bottom__boxLeft__nav__title">Início</h4>
                             <ul class="px-0 mb-0">
@@ -65,14 +68,14 @@
                         @endforeach
 
                     </div>
-                    <div class="foot03__bottom__boxRight col-sm-5  d-flex px-0 mx-0">
+                    <div class="foot03__bottom__boxRight col-sm-4  d-flex px-0 mx-0">
                         <nav class="foot03__bottom__boxRight__nav col px-0">
                             <h4 class="foot03__bottom__boxRight__nav__title">Unidades</h4>
                             <ul class="px-0 mb-0">
                                 <li>
                                     <a href="#" rel="next" class="d-flex align-items-center">
                                         <div class="icone">
-                                            <img src="{{asset('storage/uploads/tmp/logo-two.png')}}" alt="">
+                                            <img src="{{asset('storage/uploads/tmp/icon-gray.png')}}" alt="">
                                         </div>
                                         <div class="description">
                                             <h4 class="title">Unidade</h4>
@@ -82,13 +85,13 @@
                                 </li>
                             </ul>
                         </nav>
-                        <nav class="foot03__bottom__boxRight__nav col px-0">
+                        <nav class="foot03__bottom__boxRight__nav col-auto   px-0">
                             <h4 class="foot03__bottom__boxRight__nav__title">Funcionamento</h4>
                             <ul class="mb-0">
                                 <li>
                                     <a href="#" rel="next" class="d-flex align-items-center">
                                         <div class="icone">
-                                            <img src="{{asset('storage/uploads/tmp/logo-two.png')}}" alt="">
+                                            <img src="{{asset('storage/uploads/tmp/icon-gray.png')}}" alt="">
                                         </div>
                                         <div class="description">
                                             <h4 class="title">Unidade</h4>
@@ -118,7 +121,7 @@
                     </nav>
                     <div class="foot03__credits__logo">
                         <a href="#" rel="next">
-                            <img src="{{asset('storage/uploads/tmp/logo.png')}}" class="w-100 h-100" alt="Logo">
+                            <img src="{{asset('storage/uploads/tmp/logo_dark.png')}}" class="w-100 h-100" alt="Logo">
                         </a>
                     </div>
                 </div>
