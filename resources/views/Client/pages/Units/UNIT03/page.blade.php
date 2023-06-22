f872910ddfb71f434b54079df564e50c54b0ba61
ia5go <benvindoiago@outlook.com>
2023-06-22 11:35:08 -0300

    finalizado verificação dos módulos
    Falta apenas a revisão do SCHE01

--- resources/views/Client/pages/Units/UNIT03/page.blade.php
+++ resources/views/Client/pages/Units/UNIT03/page.blade.php
@@ -4,11 +4,12 @@
     <section class="unit03-page w-100">
         @if ($banner)
             <header class="unit03-page__banner"
-                style="background-image: url({{ asset('storage/' . $banner->path_image_desktop) }}); background-color: {{$banner->background_color}}">
-                <div class="unit03-page__banner__content container d-flex flex-column align-items-center justify-content-center">
+                style="background-image: url({{ asset('storage/' . $banner->path_image_desktop) }}); background-color: {{ $banner->background_color }}">
+                <div
+                    class="unit03-page__banner__content container d-flex flex-column align-items-center justify-content-center">
                     @if ($banner->title || $banner->subtitle)
-                        <h1 class="unit03-page__banner__title text-center">{{$banner->title}}</h1>
-                        <h2 class="unit03-page__banner__subtitle text-center">{{$banner->subtitle}}</h2>
+                        <h1 class="unit03-page__banner__title text-center">{{ $banner->title }}</h1>
+                        <h2 class="unit03-page__banner__subtitle text-center">{{ $banner->subtitle }}</h2>
                         <hr class="unit03-page__banner__line">
                     @endif
                 </div>
@@ -18,14 +19,15 @@
             <div class="container d-flex flex-column align-items-stretch">
                 @if ($categories->count())
                     <ul class="unit03-page__categories w-100">
-                        @foreach ( $categories as $category)
-                            <li class="unit03-page__categories__item {{isset($category->selected) ? 'active':''}}">
-                                <a href="{{route('unit03.category.page', ['UNIT03UnitsCategory' => $category->slug])}}">
+                        @foreach ($categories as $category)
+                            <li class="unit03-page__categories__item {{ isset($category->selected) ? 'active' : '' }}">
+                                <a href="{{ route('unit03.category.page', ['UNIT03UnitsCategory' => $category->slug]) }}">
                                     @if ($category->path_image_icon)
-                                        <img src="{{ asset('storage/' . $category->path_image_icon) }}" alt="" class="unit03-page__categories__item__icon">
+                                        <img src="{{ asset('storage/' . $category->path_image_icon) }}" alt=""
+                                            class="unit03-page__categories__item__icon">
                                     @endif
                                     @if ($category->title)
-                                        {{$category->title}}
+                                        {{ $category->title }}
                                     @endif
                                 </a>
                             </li>
@@ -49,12 +51,14 @@
                                         <ul>
                                             @foreach ($categories as $category)
                                                 <li class="unit03-page__categories__dropdown-mobile__item">
-                                                    <a href="{{route('unit03.category.page', ['UNIT03UnitsCategory' => $category->slug])}}">
+                                                    <a
+                                                        href="{{ route('unit03.category.page', ['UNIT03UnitsCategory' => $category->slug]) }}">
                                                         @if ($category->path_image_icon)
-                                                            <img src="{{ asset('storage/' . $category->path_image_icon) }}"alt="" class="unit03-page__categories__dropdown-mobile__item__icon">
+                                                            <img src="{{ asset('storage/' . $category->path_image_icon) }}"alt=""
+                                                                class="unit03-page__categories__dropdown-mobile__item__icon">
                                                         @endif
                                                         @if ($category->title)
-                                                            {{$category->title}}
+                                                            {{ $category->title }}
                                                         @endif
                                                     </a>
                                                 </li>
@@ -70,19 +74,22 @@
                     <div class="unit03-page__list w-100">
                         @foreach ($units as $unit)
                             <article class="unit03-page__list__item">
-                                <a href="{{route('unit03.page.content', ['UNIT03UnitsCategory' => $unit->category->slug, 'UNIT03Units' => $unit->slug])}}" class="link-full"></a>
+                                <a href="{{ route('unit03.page.content', ['UNIT03UnitsCategory' => $unit->category->slug, 'UNIT03Units' => $unit->slug]) }}"
+                                    class="link-full"></a>
                                 <div class="unit03-page__list__item__top">
                                     @if ($unit->path_image)
-                                        <img src="{{ asset('storage/' . $unit->path_image) }}" alt="" class="unit03-page__list__item__img">
+                                        <img src="{{ asset('storage/' . $unit->path_image) }}" alt="Thumbnail"
+                                            class="unit03-page__list__item__img">
                                     @endif
                                     @if ($unit->title)
-                                        <h2 class="unit03-page__list__item__title">{{$unit->title}}</h2>
+                                        <h2 class="unit03-page__list__item__title">{{ $unit->title }}</h2>
                                     @endif
                                 </div>
 
                                 <div class="unit03-page__list__item__bottom">
                                     @if ($unit->path_image_icon)
-                                        <img src="{{ asset('storage/' . $unit->path_image_icon) }}" alt="" class="unit03-page__list__item__icon">
+                                        <img src="{{ asset('storage/' . $unit->path_image_icon) }}" alt="Logo unidade"
+                                            class="unit03-page__list__item__icon">
                                     @endif
                                 </div>
                             </article>
--- resources/views/Client/pages/Units/UNIT03/show.blade.php
+++ resources/views/Client/pages/Units/UNIT03/show.blade.php
@@ -3,11 +3,12 @@
     {{-- BEGIN Page content --}}
     @if ($bannerShow)
         <section class="unit03-show__banner"
-        style="background-image: url({{ asset('storage/' . $bannerShow->path_image_desktop) }}); background-color: {{$bannerShow->background_color}}">
-            <header class="unit03-show__banner__content container d-flex flex-column align-items-center justify-content-center">
+            style="background-image: url({{ asset('storage/' . $bannerShow->path_image_desktop) }}); background-color: {{ $bannerShow->background_color }}">
+            <header
+                class="unit03-show__banner__content container d-flex flex-column align-items-center justify-content-center">
                 @if ($bannerShow->title || $bannerShow->subtitle)
-                    <h1 class="unit03-show__banner__title text-center">{{$bannerShow->title}}</h1>
-                    <h2 class="unit03-show__banner__subtitle text-center">{{$bannerShow->subtitle}}</h2>
+                    <h1 class="unit03-show__banner__title text-center">{{ $bannerShow->title }}</h1>
+                    <h2 class="unit03-show__banner__subtitle text-center">{{ $bannerShow->subtitle }}</h2>
                     <hr class="unit03-show__banner__line">
                 @endif
             </header>
@@ -15,29 +16,32 @@
     @endif
 
     <section class="unit03-show__top">
-        <div class="unit03-show__top__left d-flex flex-column align-items-stretch">
+        <div class="unit03-show__top__left d-flex flex-column align-items-stretch {{ $contact ? '' : ' w-100' }}">
 
             <div class="unit03-show__top__upper">
                 @if ($unit->path_image_icon_show)
-                    <img src="{{ asset('storage/' . $unit->path_image_icon_show) }}" alt="" class="unit03-show__top__img">
+                    <img src="{{ asset('storage/' . $unit->path_image_icon_show) }}" alt=""
+                        class="unit03-show__top__img">
                 @endif
 
                 <div class="unit03-show__top__ttl-grp d-flex flex-column align-items-start">
                     @if ($unit->title_show || $unit->subtitle_show)
-                        <h3 class="unit03-show__top__subtitle">{{$unit->subtitle_show}}</h3>
-                        <h2 class="unit03-show__top__title">{{$unit->title_show}}</h2>
+                        <h3 class="unit03-show__top__subtitle">{{ $unit->subtitle_show }}</h3>
+                        <h2 class="unit03-show__top__title">{{ $unit->title_show }}</h2>
                     @endif
                     <span class="unit03-show__top__category d-flex align-items-center">
-                        <img src="{{ asset('storage/' . $unit->category->path_image_icon) }}" alt="" class="unit03-show__top__category__icon">
-                        {{$unit->category->title}}
+                        <img src="{{ asset('storage/' . $unit->category->path_image_icon) }}" alt=""
+                            class="unit03-show__top__category__icon">
+                        {{ $unit->category->title }}
                     </span>
                     <hr class="unit03-show__top__line">
 
                     @if ($socials->count())
                         <ul class="unit03-show__top__social">
-                            @foreach ( $socials as $social)
+                            @foreach ($socials as $social)
                                 <li class="unit03-show__top__social__item">
-                                    <a href="{{$social->link ? getUri($social->link) : 'javascript:void(0)'}}" target="{{ $social->target_link }}" rel="prev">
+                                    <a href="{{ $social->link ? getUri($social->link) : 'javascript:void(0)' }}"
+                                        target="{{ $social->target_link }}" rel="prev">
                                         @if ($social->path_image_icon)
                                             <img src="{{ asset('storage/' . $social->path_image_icon) }}" alt="">
                                         @endif
@@ -50,16 +54,17 @@
             </div>
             @if ($topics->count())
                 <div class="unit03-show__top__carousel owl-carousel">
-                   @foreach ($topics as $topic)
+                    @foreach ($topics as $topic)
                         <div class="unit03-show__top__carousel__item d-flex justify-content-start align-items-center">
                             @if ($topic->path_image_icon)
-                                <img src="{{ asset('storage/' . $topic->path_image_icon) }}" alt="" class="unit03-show__top__carousel__item__icon">
+                                <img src="{{ asset('storage/' . $topic->path_image_icon) }}" alt=""
+                                    class="unit03-show__top__carousel__item__icon">
                             @endif
                             <div
                                 class="unit03-show__top__carousel__item__content d-flex flex-column justify-content-center align-items-start">
                                 @if ($topic->title || $topic->description)
-                                    <h3 class="unit03-show__top__carousel__item__title">{{$topic->title}}</h3>
-                                    <p class="unit03-show__top__carousel__item__desc">{!! $topic->description!!}</p>
+                                    <h3 class="unit03-show__top__carousel__item__title">{{ $topic->title }}</h3>
+                                    <p class="unit03-show__top__carousel__item__desc">{!! $topic->description !!}</p>
                                 @endif
                             </div>
                         </div>
@@ -67,11 +72,12 @@
                 </div>
             @endif
         </div>
+
         @if ($contact)
             <div class="unit03-show__top__form">
                 @if ($contact->title || $contact->subtitle)
-                    <h4 class="unit03-show__top__form__subtitle">{{$contact->subtitle}}</h4>
-                    <h3 class="unit03-show__top__form__title">{{$contact->title}}</h3>
+                    <h4 class="unit03-show__top__form__subtitle">{{ $contact->subtitle }}</h4>
+                    <h3 class="unit03-show__top__form__title">{{ $contact->title }}</h3>
                     <hr class="unit03-show__top__form__line">
                 @endif
 
@@ -84,7 +90,7 @@
                 ]) !!}
 
                 <div class="sche01-form__form__inputs d-flex flex-column w-100 align-items-stretch">
-                    <input type="hidden" name="target_lead" value="{{$contact->title}}">
+                    <input type="hidden" name="target_lead" value="{{ $contact->title }}">
                     <input type="hidden" name="target_send" value="{{ base64_encode($contact->email) }}">
 
                     @foreach ($inputs as $name => $input)
@@ -99,7 +105,8 @@
 
                 </div>
                 <button type="submit" class="unit03-show__top__form__cta">
-                    <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" class="unit03-show__top__form__cta__icon" alt="Ícone">
+                    <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}"
+                        class="unit03-show__top__form__cta__icon" alt="Ícone">
                     @if ($contact->title_button)
                         {{ $contact->title_button }}
                     @endif
@@ -111,21 +118,28 @@
 
     @if ($contents->count())
         @foreach ($contents as $content)
-            <section class="unit03-show__content" style="background-image: url({{ asset('storage/' . $content->path_image_desktop) }}); background-color: {{$content->background_color}}">
+            <section class="unit03-show__content"
+                style="background-image: url({{ asset('storage/' . $content->path_image_desktop) }}); background-color: {{ $content->background_color }}">
                 <div class="container unit03-show__content__container">
                     <div class="unit03-show__content__carousel owl-carousel">
+                        <div class="unit03-show__content__carousel__item">
+                            @if ($content->path_image)
+                                <img src="{{ asset('storage/' . $content->path_image) }}" alt="Thumbnail">
+                            @endif
+                        </div>
+
                         @foreach ($content->galleryContents as $galleryContent)
                             <div class="unit03-show__content__carousel__item">
                                 @if ($galleryContent->path_image)
-                                    <img src="{{ asset('storage/' . $galleryContent->path_image) }}" alt="">
+                                    <img src="{{ asset('storage/' . $galleryContent->path_image) }}" alt="Carousel image">
                                 @endif
                             </div>
                         @endforeach
                     </div>
                     <div class="unit03-show__content__right">
                         @if ($content->subtitle || $content->title)
-                            <h4 class="unit03-show__content__subtitle">{{$content->subtitle}}</h4>
-                            <h3 class="unit03-show__content__title">{{$content->title}}</h3>
+                            <h4 class="unit03-show__content__subtitle">{{ $content->subtitle }}</h4>
+                            <h3 class="unit03-show__content__title">{{ $content->title }}</h3>
                             <hr class="unit03-show__content__line">
                         @endif
 
@@ -137,13 +151,17 @@
                             @endif
                         </div>
 
-                        <a rel="next" href="{{$content->link_button ? getUri($content->link_button) : 'javascript:void(0)'}}" target="{{ $content->target_link_button }}" class="unit03-show__content__cta">
-                            <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
-                                class="unit03-show__content__cta__icon">
-                            @if ($content->title_button)
-                                {{$content->title_button}}
-                            @endif
-                        </a>
+                        @if ($content->link_button)
+                            <a rel="next"
+                                href="{{ $content->link_button ? getUri($content->link_button) : 'javascript:void(0)' }}"
+                                target="{{ $content->target_link_button }}" class="unit03-show__content__cta">
+                                <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
+                                    class="unit03-show__content__cta__icon">
+                                @if ($content->title_button)
+                                    {{ $content->title_button }}
+                                @endif
+                            </a>
+                        @endif
                     </div>
                 </div>
             </section>
@@ -155,8 +173,8 @@
             @if ($sectionGallery)
                 <header class="unit03-show__gallery__header d-flex flex-column align-items-center">
                     @if ($sectionGallery->title || $sectionGallery->subtitle)
-                        <h4 class="unit03-show__gallery__subtitle">{{$sectionGallery->subtitle}}</h4>
-                        <h3 class="unit03-show__gallery__title">{{$sectionGallery->title}}</h3>
+                        <h4 class="unit03-show__gallery__subtitle">{{ $sectionGallery->subtitle }}</h4>
+                        <h3 class="unit03-show__gallery__title">{{ $sectionGallery->title }}</h3>
                         <hr class="unit03-show__gallery__line">
                     @endif
                 </header>
@@ -165,21 +183,24 @@
             @if ($galleries->count())
                 <div class="unit03-show__gallery__list">
                     @foreach ($galleries as $gallery)
-                        <div class="unit03-show__gallery__item {{ $gallery->link_video ? ' play-button' : '' }}" >
-                            <a href="{{ getUri($gallery->link_video != '' ? $gallery->link_video : asset('storage/' . $gallery->path_image)) }}" data-fancybox>
-                                <img src="{{ asset('storage/' . $gallery->path_image) }}" class="h-100 w-100 "  alt="">
+                        <div class="unit03-show__gallery__item {{ $gallery->link_video ? ' play-button' : '' }}">
+                            <a class="link-full"
+                                href="{{ getUri($gallery->link_video != '' ? $gallery->link_video : asset('storage/' . $gallery->path_image)) }}"
+                                data-fancybox>
                             </a>
+                            <img src="{{ asset('storage/' . $gallery->path_image) }}" class="h-100 w-100 " alt="">
                             @if ($gallery->title)
-                                <h3 class="unit03-show__gallery__item__title">{{$gallery->title}}</h3>
+                                <h3 class="unit03-show__gallery__item__title">{{ $gallery->title }}</h3>
                             @endif
                         </div>
                     @endforeach
-                    <div class="unit03-show__gallery__item--big {{ $unit->link_video ? ' play-button' : '' }}" >
-                        <a href="{{ getUri($unit->link_video != '' ? $unit->link_video : asset('storage/' . $unit->path_image_gallery)) }}" data-fancybox>
-                            <img src="{{ asset('storage/' . $unit->path_image_gallery) }}" class="h-100 w-100" alt="">
+                    <div class="unit03-show__gallery__item--big {{ $unit->link_video ? ' play-button' : '' }}">
+                        <a href="{{ getUri($unit->link_video != '' ? $unit->link_video : asset('storage/' . $unit->path_image_gallery)) }}"
+                            class="link-full" data-fancybox>
                         </a>
+                        <img src="{{ asset('storage/' . $unit->path_image_gallery) }}" class="h-100 w-100" alt="">
                         @if ($unit->title_gallery)
-                            <h3 class="unit03-show__gallery__item__title">{{$unit->title_gallery}}</h3>
+                            <h3 class="unit03-show__gallery__item__title">{{ $unit->title_gallery }}</h3>
                         @endif
                     </div>
                 </div>
