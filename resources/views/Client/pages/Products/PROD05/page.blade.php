3f9d2de7dcfabce8dffa6609d40b48c6139c4c2d
ia5go <benvindoiago@outlook.com>
2023-06-23 11:33:18 -0300

    desenvolvendo a page
    Parei pois arrumaram o repositório que eu precisava para continuar trabalhando em Marcado Santo Amaro

--- resources/views/Client/pages/Products/PROD05/page.blade.php
+++ resources/views/Client/pages/Products/PROD05/page.blade.php
@@ -6,31 +6,132 @@
 
         <header class="prod05-page__header w-100">
 
-            <div class="serv05-banner-carousel owl-carousel w-100">
+            <div class="prod05-banner w-100"
+                style="background-image: url({{ asset('storage/uploads/tmp/bg-banner-inner.jpg') }})">
+                <div class="container d-flex flex-column align-items-center justify-content-center">
+                    <h3 class="prod05-banner__title text-center">Título Banner</h3>
+                    <h4 class="prod05-banner__subtitle text-center">Subtítulo</h4>
+                    <hr class="prod05-banner__line">
 
-                @for ($i = 0; $i < 3; $i++)
-                    <div class="serv05-banner-carousel__item"
-                        style="background-image: url({{ asset('storage/uploads/tmp/bg-banner-inner.jpg') }})">
-                        <div class="container d-flex flex-column align-items-center justify-content-center">
-                            <h3 class="serv05-banner-carousel__title text-center">Título Página {{ $i }}</h3>
-                            <h4 class="serv05-banner-carousel__subtitle text-center">Subtítulo</h4>
-                            <hr class="serv05-banner-carousel__line">
+                    {{-- CATEGORIAS --}}
+                    <div class="prod05-categories">
+
+                        <ul class="prod05-categories__list w-100">
+                            <li class="active prod05-categories__list__item">
+                                <a href="">
+                                    <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
+                                        class="prod05-categories__list__item__icon">
+                                    Categoria
+                                </a>
+                            </li>
+                            @for ($i = 0; $i < 3; $i++)
+                                <li class="prod05-categories__list__item">
+                                    <a href="">
+                                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
+                                            class="prod05-categories__list__item__icon">
+                                        Categoria
+                                    </a>
+                                </li>
+                            @endfor
+                        </ul>
+
+                        <div class="prod05-categories__dropdown-mobile">
+                            <div class="accordion accordion-flush" id="accordionFlushExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header prod05-categories__dropdown-mobile__item">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                            aria-controls="flush-collapseOne">
+                                            <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
+                                                class="prod05-categories__dropdown-mobile__item__icon">
+                                            Categoria
+                                        </button>
+                                    </h2>
+                                    <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                        data-bs-parent="#accordionFlushExample">
+                                        <div class="accordion-body">
+                                            <ul>
+                                                @for ($i = 0; $i < 3; $i++)
+                                                    <li class="prod05-categories__dropdown-mobile__item">
+                                                        <a href="">
+                                                            <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}"
+                                                                alt=""
+                                                                class="prod05-categories__dropdown-mobile__item__icon">
+                                                            Categoria
+                                                        </a>
+                                                    </li>
+                                                @endfor
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                @endfor
 
+                    </div>
+                </div>
             </div>
 
-            <div class="serv05-top w-100">
+            <div class="prod05-top w-100">
                 <div class="container d-flex flex-column align-items-center justify-content-center">
-                    <h1 class="serv05-top__title text-center">Título Página {{ $i }}</h1>
-                    <h2 class="serv05-top__subtitle text-center">Subtítulo</h2>
-                    <hr class="serv05-top__line">
-                    <div class="serv05-top__desc">
+                    <h1 class="prod05-top__title text-center">Título Página</h1>
+                    <h2 class="prod05-top__subtitle text-center">Subtítulo</h2>
+                    <hr class="prod05-top__line">
+                    <div class="prod05-top__desc">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel tortor eu purus gravida
                             sollicitudin vel non libero. Vivamus commodo porta velit, vel tempus mi pretium sed. In et arcu
                             eget purus mattis posuere. Donec tincidunt dignissim faucibus. Lorem ipsum dolor sit amet,
                             consectetur adipiscing elit. Cras vel tortor</p>
+                    </div>
+
+                    {{-- SUBCATEGORIAS --}}
+                    <div class="prod05-categories">
+
+                        <ul class="prod05-categories__list w-100">
+                            @for ($i = 0; $i < 3; $i++)
+                                <li class="prod05-categories__list__item">
+                                    <a href="">
+                                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
+                                            class="prod05-categories__list__item__icon">
+                                        Subcategoria
+                                    </a>
+                                </li>
+                            @endfor
+                        </ul>
+
+                        <div class="prod05-categories__dropdown-mobile">
+                            <div class="accordion accordion-flush" id="accordionFlushExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header prod05-categories__dropdown-mobile__item">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                            aria-controls="flush-collapseOne">
+                                            <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
+                                                class="prod05-categories__dropdown-mobile__item__icon">
+                                            Categoria
+                                        </button>
+                                    </h2>
+                                    <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                        data-bs-parent="#accordionFlushExample">
+                                        <div class="accordion-body">
+                                            <ul>
+                                                @for ($i = 0; $i < 3; $i++)
+                                                    <li class="prod05-categories__dropdown-mobile__item">
+                                                        <a href="">
+                                                            <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}"
+                                                                alt=""
+                                                                class="prod05-categories__dropdown-mobile__item__icon">
+                                                            Categoria
+                                                        </a>
+                                                    </li>
+                                                @endfor
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -40,91 +141,32 @@
         <main class="prod05-page__main">
             <div class="container d-flex flex-column align-items-center">
 
-                <div class="serv05-categories">
-
-                    <ul class="serv05-categories__list w-100">
-                        @for ($i = 0; $i < 3; $i++)
-                            <li class="serv05-categories__list__item">
-                                <a href="">
-                                    <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
-                                        class="serv05-categories__list__item__icon">
-                                    Categoria
-                                </a>
-                            </li>
-                        @endfor
-                    </ul>
-
-                    <div class="serv05-categories__dropdown-mobile">
-                        <div class="accordion accordion-flush" id="accordionFlushExample">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header serv05-categories__dropdown-mobile__item">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseOne" aria-expanded="false"
-                                        aria-controls="flush-collapseOne">
-                                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
-                                            class="serv05-categories__dropdown-mobile__item__icon">
-                                        Categoria
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseOne" class="accordion-collapse collapse"
-                                    data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                        <ul>
-                                            @for ($i = 0; $i < 3; $i++)
-                                                <li class="serv05-categories__dropdown-mobile__item">
-                                                    <a href="">
-                                                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}"
-                                                            alt=""
-                                                            class="serv05-categories__dropdown-mobile__item__icon">
-                                                        Categoria
-                                                    </a>
-                                                </li>
-                                            @endfor
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
 
                 <div class="prod05-page__main__list">
 
-                    @for ($i = 0; $i < 8; $i++)
-                        <article class="serv05-box"
-                            style="background-image: url({{ asset('storage/uploads/tmp/bg-boxitem.png') }})">
+                    @for ($i = 0; $i < 6; $i++)
+                        <article class=" prod05-box">
 
-                            <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
-                                class="serv05-box__icon">
+                            <img src="{{ asset('storage/uploads/tmp/thumbnail.png') }}" alt=""
+                                class="prod05-box__image">
 
-                            <div class="serv05-box__content w-100 d-flex flex-column align-items-stretch">
-                                <div class="serv05-box__top w-100 d-flex align-items-center justify-content-between">
-                                    <div
-                                        class="serv05-box__top__left d-flex flex-column align-items-start justify-content-start ">
+                            <div class="prod05-box__content w-100 d-flex flex-column align-items-center">
+                                <div class="prod05-box__top w-100 d-flex flex-column align-items-center">
 
-                                        <h3 class="serv05-box__top__title">Título</h3>
-                                        <h4 class="serv05-box__top__subtitle">Subtitulo</h4>
+                                    <h3 class="prod05-box__top__title">Título</h3>
+                                    <h4 class="prod05-box__top__subtitle">Subtitulo</h4>
 
-                                    </div>
-
-                                    <div
-                                        class="serv05-box__top__right d-flex flex-column align-items-end justify-content-start ">
-                                        <h4 class="serv05-box__top__subtitle">Subtitulo</h4>
-                                        <h3 class="serv05-box__top__title">00,00</h3>
-                                    </div>
                                 </div>
 
-                                <hr class="serv05-box__line">
+                                <hr class="prod05-box__line">
 
-                                <p class="serv05-box__desc">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. olor sit amet, consectetur
-                                    adipiscing elit.
+                                <p class="prod05-box__desc">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                                 </p>
 
-                                <a href="#" class="serv05-box__cta">
+                                <a href="#" class="prod05-box__cta">
                                     <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt=""
-                                        class="serv05-box__cta__icon">
+                                        class="prod05-box__cta__icon">
                                     CTA
                                 </a>
                             </div>
