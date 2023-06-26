d14474ab2263622f3e7d22d926423df995352c2b
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2023-06-26 16:36:04 -0300

    Update form.blade

--- resources/views/Admin/cruds/Schedules/SCHE01/form.blade.php
+++ resources/views/Admin/cruds/Schedules/SCHE01/form.blade.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-12 col-sm-6">
                     {!! Form::label('title', 'Título', ['class' => 'form-label']) !!}
-                    {!! Form::text('title', null, ['class' => 'form-control', 'id' => 'title']) !!}
+                    {!! Form::text('title', null, ['class' => 'form-control', 'id' => 'title', 'required'=>'required',]) !!}
                 </div>
                 <div class="col-12 col-sm-6">
                     {!! Form::label('subtitle', 'Subtítulo', ['class' => 'form-label']) !!}
