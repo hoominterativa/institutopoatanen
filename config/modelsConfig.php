0fca7c68bc8dee7e18e09db2689ad241d8a97e81
Anderson de Souza Viana <63976965+nandosDev@users.noreply.github.com>
2023-06-28 11:48:26 -0300

    Update modelsConfig.php

--- config/modelsConfig.php
+++ config/modelsConfig.php
@@ -4,33 +4,14 @@ return [
     // Configure the model the header and footer, not change the module
     'InsertModelsCore' => (object)[
         'Headers' => (object)[
-            'Code' => 'HEAD02',
-            'themeMenu' => 'SIDE02'
         ],
         'Footers' => (object)[
-            'Code' => 'FOOT02'
         ]
     ],
 
     // Configure existing modules and templates site-wide/system
     'InsertModelsMain' => (object) [
-        'Products' => (object) [
-            'PROD05' => (object)[
-                'ViewHome' => true,
-                'ViewListMenu' => false,
-                'ViewListPanel' => true,
-                'ViewListFooter' => false,
-                'Viewer' => 'dropdown', // accepted values, list or dropdown
-                'config' => (object) [
-                    'titleMenu' => '',
-                    'anchor' => false,
-                    'linkMenu' => '',
-                    'iconMenu' => '',
-                    'titlePanel' => 'Produtos',
-                    'iconPanel' => ''
-                ]
-            ],
-        ],
+
     ],
 
     'ModelsForm' => (object)[
