fe5bd48a0c55c479a730ada229552482ae13448f
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2023-07-11 09:48:05 -0300

    Update SLID03

--- resources/views/Admin/cruds/Slides/SLID03/form.blade.php
+++ resources/views/Admin/cruds/Slides/SLID03/form.blade.php
@@ -6,6 +6,18 @@
     <div class="row col-12">
         <div class="col-12 col-lg-6">
             <div class="card card-body" id="tooltip-container">
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            {!! Form::label('title', 'Título', ['class' => 'form-label']) !!}
+                            {!! Form::text('title', null, ['class' => 'form-control', 'required'=>'required', 'id' => 'title']) !!}
+                        </div>
+                        <div class="col-sm-6">
+                            {!! Form::label('subtitle', 'Subtítulo', ['class' => 'form-label']) !!}
+                            {!! Form::text('subtitle', null, ['class' => 'form-control', 'id' => 'subtitle']) !!}
+                        </div>
+                    </div>
+                </div>
                 <div class="complete-editor__content mb-3">
                     {!! Form::label('complete-editor', 'Descrição', ['class'=>'form-label']) !!}
                     {!! Form::textarea('description', null, [
