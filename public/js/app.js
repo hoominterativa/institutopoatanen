95e0a02622a38316dc39784869d2ffaa876d6b90
jocleiton-paixao <frontend8@hoom.com.br>
2023-08-04 09:10:20 -0300

    Ajustes Frontend

--- public/js/app.js
+++ public/js/app.js
@@ -12116,6 +12116,33 @@ $(function () {
     });
   }
 });
+$(function () {
+  if ($(window).outerWidth()) {
+    $('.carousel-serv07-show-section-product').addClass('owl-carousel');
+    $('.carousel-serv07-show-section-product').owlCarousel({
+      margin: 0,
+      stagePadding: 0,
+      smartSpeed: 450,
+      dots: true,
+      nav: false,
+      rewind: true,
+      responsive: {
+        // breakpoint from 0 up
+        0: {
+          items: 1
+        },
+        // breakpoint from 360 up
+        361: {
+          items: 1
+        },
+        // breakpoint from 768 up
+        800: {
+          items: 5
+        }
+      }
+    });
+  }
+});
 
 /***/ }),
 
--- resources/views/Client/pages/Services/SERV07/show.blade.php
+++ resources/views/Client/pages/Services/SERV07/show.blade.php
@@ -2,6 +2,216 @@
 @section('content')
 {{-- BEGIN Page content --}}
 
+<div class="serv07-show-banner">    
+    <header class="serv07-show-banner__header"style="background-image: url(#); background-color: #00000070;">
+        <div class="container d-flex flex-column align-items-center">
+            <h1 class="serv07-show-banner__title">Title</h1>
+        </div>
+    </header>
+</div>
+
+
+<!-- TEAM01 -->
+<div class="serv07-show-section__content__product container">
+    <header class="header-topic">
+        <h3 class="container-title">
+            <span class="title">title</span>
+        </h3>
+        <hr class="line">
+    </header>
+    <div class="row serv07-show-section__content--row carousel-serv07-show-section-product">
+        <article class="serv07-show-section__content__product__item col-md-3">
+            <div class="serv07-show-section__content__product__item__image">
+                <img src="##" class="w-100 h-100" alt="Titulo Topico">
+            </div>
+            <div class="serv07-show-section__content__product__item__description d-flex  flex-column justify-content-end mx-0 w-100 text-center">
+                <div class="serv07-show-section__content__product__item__description__encompass">
+                    <div class="flex-column serv07-show-section__content__product__item__description__encompass__txt">
+                        <h2 class="serv07-show-section__content__product__item__description__encompass__txt__title mx-0 px-0">title</h2>
+                    </div>
+                </div>
+                <div class="serv07-show-section__content__product__item__description_paragraph text-start px-0 ">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, facere.
+                    </p>
+                </div>
+                <div class="serv07-show-section__content__product__item__description__buttons">
+                    <a rel="next" href="#" data-fancybox= "#" data-src="#"  class="serv07-show-section__content__product__item__description__buttons__cta transition d-flex justify-content-center align-items-center mx-auto">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__product__item__description__buttons__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </article>
+        <article class="serv07-show-section__content__product__item col-md-3">
+            <div class="serv07-show-section__content__product__item__image">
+                <img src="##" class="w-100 h-100" alt="Titulo Topico">
+            </div>
+            <div class="serv07-show-section__content__product__item__description d-flex  flex-column justify-content-end mx-0 w-100 text-center">
+                <div class="serv07-show-section__content__product__item__description__encompass">
+                    <div class="flex-column serv07-show-section__content__product__item__description__encompass__txt">
+                        <h2 class="serv07-show-section__content__product__item__description__encompass__txt__title mx-0 px-0">title</h2>
+                    </div>
+                </div>
+                <div class="serv07-show-section__content__product__item__description_paragraph text-start px-0 ">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, facere.
+                    </p>
+                </div>
+                <div class="serv07-show-section__content__product__item__description__buttons">
+                    <a rel="next" href="#" data-fancybox= "#" data-src="#"  class="serv07-show-section__content__product__item__description__buttons__cta transition d-flex justify-content-center align-items-center mx-auto">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__product__item__description__buttons__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </article>
+        <article class="serv07-show-section__content__product__item col-md-3">
+            <div class="serv07-show-section__content__product__item__image">
+                <img src="##" class="w-100 h-100" alt="Titulo Topico">
+            </div>
+            <div class="serv07-show-section__content__product__item__description d-flex  flex-column justify-content-end mx-0 w-100 text-center">
+                <div class="serv07-show-section__content__product__item__description__encompass">
+                    <div class="flex-column serv07-show-section__content__product__item__description__encompass__txt">
+                        <h2 class="serv07-show-section__content__product__item__description__encompass__txt__title mx-0 px-0">title</h2>
+                    </div>
+                </div>
+                <div class="serv07-show-section__content__product__item__description_paragraph text-start px-0 ">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, facere.
+                    </p>
+                </div>
+                <div class="serv07-show-section__content__product__item__description__buttons">
+                    <a rel="next" href="#" data-fancybox= "#" data-src="#"  class="serv07-show-section__content__product__item__description__buttons__cta transition d-flex justify-content-center align-items-center mx-auto">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__product__item__description__buttons__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </article>
+        <article class="serv07-show-section__content__product__item col-md-3">
+            <div class="serv07-show-section__content__product__item__image">
+                <img src="##" class="w-100 h-100" alt="Titulo Topico">
+            </div>
+            <div class="serv07-show-section__content__product__item__description d-flex  flex-column justify-content-end mx-0 w-100 text-center">
+                <div class="serv07-show-section__content__product__item__description__encompass">
+                    <div class="flex-column serv07-show-section__content__product__item__description__encompass__txt">
+                        <h2 class="serv07-show-section__content__product__item__description__encompass__txt__title mx-0 px-0">title</h2>
+                    </div>
+                </div>
+                <div class="serv07-show-section__content__product__item__description_paragraph text-start px-0 ">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, facere.
+                    </p>
+                </div>
+                <div class="serv07-show-section__content__product__item__description__buttons">
+                    <a rel="next" href="#" data-fancybox= "#" data-src="#"  class="serv07-show-section__content__product__item__description__buttons__cta transition d-flex justify-content-center align-items-center mx-auto">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__product__item__description__buttons__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </article>
+        <article class="serv07-show-section__content__product__item col-md-3">
+            <div class="serv07-show-section__content__product__item__image">
+                <img src="##" class="w-100 h-100" alt="Titulo Topico">
+            </div>
+            <div class="serv07-show-section__content__product__item__description d-flex  flex-column justify-content-end mx-0 w-100 text-center">
+                <div class="serv07-show-section__content__product__item__description__encompass">
+                    <div class="flex-column serv07-show-section__content__product__item__description__encompass__txt">
+                        <h2 class="serv07-show-section__content__product__item__description__encompass__txt__title mx-0 px-0">title</h2>
+                    </div>
+                </div>
+                <div class="serv07-show-section__content__product__item__description_paragraph text-start px-0 ">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, facere.
+                    </p>
+                </div>
+                <div class="serv07-show-section__content__product__item__description__buttons">
+                    <a rel="next" href="#" data-fancybox= "#" data-src="#"  class="serv07-show-section__content__product__item__description__buttons__cta transition d-flex justify-content-center align-items-center mx-auto">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__product__item__description__buttons__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </article>
+        <article class="serv07-show-section__content__product__item col-md-3">
+            <div class="serv07-show-section__content__product__item__image">
+                <img src="##" class="w-100 h-100" alt="Titulo Topico">
+            </div>
+            <div class="serv07-show-section__content__product__item__description d-flex  flex-column justify-content-end mx-0 w-100 text-center">
+                <div class="serv07-show-section__content__product__item__description__encompass">
+                    <div class="flex-column serv07-show-section__content__product__item__description__encompass__txt">
+                        <h2 class="serv07-show-section__content__product__item__description__encompass__txt__title mx-0 px-0">title</h2>
+                    </div>
+                </div>
+                <div class="serv07-show-section__content__product__item__description_paragraph text-start px-0 ">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, facere.
+                    </p>
+                </div>
+                <div class="serv07-show-section__content__product__item__description__buttons">
+                    <a rel="next" href="#" data-fancybox= "#" data-src="#"  class="serv07-show-section__content__product__item__description__buttons__cta transition d-flex justify-content-center align-items-center mx-auto">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__product__item__description__buttons__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </article>
+        <article class="serv07-show-section__content__product__item col-md-3">
+            <div class="serv07-show-section__content__product__item__image">
+                <img src="##" class="w-100 h-100" alt="Titulo Topico">
+            </div>
+            <div class="serv07-show-section__content__product__item__description d-flex  flex-column justify-content-end mx-0 w-100 text-center">
+                <div class="serv07-show-section__content__product__item__description__encompass">
+                    <div class="flex-column serv07-show-section__content__product__item__description__encompass__txt">
+                        <h2 class="serv07-show-section__content__product__item__description__encompass__txt__title mx-0 px-0">title</h2>
+                    </div>
+                </div>
+                <div class="serv07-show-section__content__product__item__description_paragraph text-start px-0 ">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, facere.
+                    </p>
+                </div>
+                <div class="serv07-show-section__content__product__item__description__buttons">
+                    <a rel="next" href="#" data-fancybox= "#" data-src="#"  class="serv07-show-section__content__product__item__description__buttons__cta transition d-flex justify-content-center align-items-center mx-auto">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__product__item__description__buttons__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </article>
+        <article class="serv07-show-section__content__product__item col-md-3">
+            <div class="serv07-show-section__content__product__item__image">
+                <img src="##" class="w-100 h-100" alt="Titulo Topico">
+            </div>
+            <div class="serv07-show-section__content__product__item__description d-flex  flex-column justify-content-end mx-0 w-100 text-center">
+                <div class="serv07-show-section__content__product__item__description__encompass">
+                    <div class="flex-column serv07-show-section__content__product__item__description__encompass__txt">
+                        <h2 class="serv07-show-section__content__product__item__description__encompass__txt__title mx-0 px-0">title</h2>
+                    </div>
+                </div>
+                <div class="serv07-show-section__content__product__item__description_paragraph text-start px-0 ">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, facere.
+                    </p>
+                </div>
+                <div class="serv07-show-section__content__product__item__description__buttons">
+                    <a rel="next" href="#" data-fancybox= "#" data-src="#"  class="serv07-show-section__content__product__item__description__buttons__cta transition d-flex justify-content-center align-items-center mx-auto">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__product__item__description__buttons__cta__icon me-3 transition">
+                        CTA
+                    </a>
+                </div>
+            </div>
+        </article>
+    </div>
+    <a href="#" class="serv07-show-section__content__cta">
+        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv07-show-section__content__cta__icon">
+        CTA
+    </a>
+</div>
+
+
 {{-- Finish Content page Here --}}
 @foreach ($sections as $section)
     {!!$section!!}
