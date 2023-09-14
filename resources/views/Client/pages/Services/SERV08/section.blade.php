e7575f63e7bd95bfe9c1d06661a2fb7fb1874028
Abne Mescena <136840163+MescenaFront@users.noreply.github.com>
2023-09-14 08:24:00 -0300

    Montagem SERV08

---
+++ resources/views/Client/pages/Services/SERV08/section.blade.php
@@ -0,0 +1,75 @@
+<section id="SERV08" class="serv08">
+    <div class="container">
+        <header class="serv08__header d-flex flex-column align-items-center">
+            <h2 class="serv08__title"></h2>
+            <h3 class="serv08__subtitle"></h3>
+            <hr class="serv08__line">
+            <p class="serv08__desc">
+            </p>
+            <div class="serv08-categories">
+                <ul class="serv08-categories__list w-100">
+                    <li class="serv08-categories__list__item">
+                        <a href="">
+                            <img src="" alt="" class="serv08-categories__list__item__icon">
+                        </a>
+                    </li>
+                </ul>
+                <div class="serv08-categories__dropdown-mobile">
+                    <div class="accordion accordion-flush" id="accordionFlushExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header serv08-categories__dropdown-mobile__item">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv08-categories__dropdown-mobile__item__icon">
+                                    Categorias
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">
+                                    <ul>
+
+                                        <li class="serv08-categories__dropdown-mobile__item">
+                                            <a href="">
+                                                <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv08-categories__dropdown-mobile__item__icon">
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main class="serv08__main w-100 d-flex flex-column align-items-stretch">
+            <div class="serv08__carousel owl-carousel">
+                <article class="serv08__carousel__item serv08-box" style="">
+                    <img src="" alt="" class="serv08-box__icon">
+                    <div class="serv08-box__content w-100 d-flex flex-column align-items-stretch">
+                        <div class="serv08-box__top w-100 d-flex align-items-center justify-content-between">
+                            <div class="serv08-box__top__left d-flex flex-column align-items-start justify-content-start ">
+                                <h3 class="serv08-box__top__title"></h3>
+                                <h4 class="serv08-box__top__subtitle"></h4>
+                            </div>
+                            <div class="serv08-box__top__right d-flex flex-column align-items-end justify-content-start ">
+                                <h4 class="serv08-box__top__subtitle"></h4>
+                                <h3 class="serv08-box__top__title">R$ </h3>
+                            </div>
+                        </div>
+                        <hr class="serv08-box__line">
+                        <p class="serv08-box__desc">
+                        </p>
+                        <a href="" class="serv08-box__cta">
+                            <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv08-box__cta__icon">
+                            CTA
+                        </a>
+                    </div>
+                </article>
+            </div>
+            <a href="" class="serv08__cta">
+                <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="" class="serv08__cta__icon">
+                CTA
+            </a>
+        </main>
+
+    </div>
+</section>
