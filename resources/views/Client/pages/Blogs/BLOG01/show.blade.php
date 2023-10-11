f2b64b0d913518ce5105c5fb74f1c59124997f0a
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2023-10-11 10:24:20 -0300

    Update

--- resources/views/Client/pages/Blogs/BLOG01/show.blade.php
+++ resources/views/Client/pages/Blogs/BLOG01/show.blade.php
@@ -18,7 +18,7 @@
                                 {!!$blog->text!!}
                             </div>
                             <div class="d-flex justify-content-end align-items-end">
-                                <a href="" class="blog01-show__item__share" id="shareButton">Compartilhar Artigo</a> 
+                                <a href="" class="blog01-show__item__share" id="shareButton">Compartilhar Artigo</a>
                             </div>
                         </article>
                     </div>
@@ -59,8 +59,30 @@
     @foreach ($sections as $section)
         {!!$section!!}
     @endforeach
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
+@endsection
+
+@section('tagSeo')
+    <title>{{$blog->title}}</title>
+    <meta name="title" content="{{$blog->title}}">
+    <meta name="description" content="{{ $blog->description }}">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="article">
+    <meta property="og:url" content="{{url()->current()}}">
+    <meta property="og:title" content="{{$blog->title}}">
+    <meta property="og:description" content="{{$blog->description}}">
+    <meta property="og:image" content="{{asset('storage/'.$blog->path_image_thumbnail)}}">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="{{url()->current()}}">
+    <meta property="twitter:title" content="{{$blog->title}}">
+    <meta property="twitter:description" content="{{substr($blog->description, 0 , 130)}}">
+    <meta property="twitter:image" content="{{asset('storage/'.$blog->path_image_thumbnail)}}">
+@endsection
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
         const shareButton = document.getElementById("shareButton");
 
         shareButton.addEventListener("click", function() {
@@ -91,5 +113,4 @@
             }
         });
     });
-    </script>
-@endsection
+</script>
