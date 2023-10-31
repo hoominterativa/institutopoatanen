1f0e97290eb23c240fcbad9beb23c0dc73474d0f
WilliamWatanabeFront <frontend3@hoom.com.br>
2023-10-31 11:57:27 -0300

    Update: PORT04-PAGE
    > Configuração da page para web-view;

--- resources/views/Client/pages/Portfolios/PORT04/page.blade.php
+++ resources/views/Client/pages/Portfolios/PORT04/page.blade.php
@@ -1,87 +1,107 @@
 @extends('Client.Core.client')
 @section('content')
-{{-- BEGIN Page content --}}
-
-<section class="port04-page">
-    <header class="">
-        <div class="">
-            @if ($section->active_banner)
-                <div class="" style="background-image: url({{ asset('storage/' . $section->path_image_desktop_banner) }});  background-color: {{$section->background_color_banner}};">
-                    @if ($section->title_banner || $section->subtitle_banner)
-                        <div class="">
-                            <h3 class="">{{$section->title_banner}}</h3>
-                            <h4 class="">{{$section->subtitle_banner}}</h4>
-                            <hr class="">
-                        </div>
-                    @endif
-                </div>
-            @endif
-        </div>
-    </header>
-    <main class="">
-        <div class="">
-            <div class="">
-                <div class="">
-                    @if ($section->active_content)
-                        @if ($section->subtitle_content || $section->title_content)
-                            <h1 class="">{{$section->subtitle_content}}</h1>
-                            <h2 class="">{{$section->title_content}}</h2>
-                            <hr class="">
-                        @endif
-                        @if ($section->text_content)
-                            <div class="">
-                                <p>
-                                    {!! $section->text_content !!}
-                                </p>
-                            </div>
-                        @endif
-                    @endif
-                    @if ($categories->count())
-                        <div class="">
-                            <ul class="">
-                                @foreach ($categories as $category)
-                                    <li class=" {{isset($category->selected) ? 'active':''}}">
-                                        <a href="{{route('port04.category.page', ['PORT04PortfoliosCategory' =>$category->slug])}}">
-                                            @if ($category->path_image_icon)
-                                                <img src="{{ asset('storage/' . $category->path_image_icon) }}" alt="Ícone da categoria" class="">
-                                            @endif
-                                            {{$category->title}}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                </div>
-            </div>
-            @if ($portfolios->count())
-                <div class="">
-                    @foreach ($portfolios as $portfolio)
-                        <article>
-                            <a href="{{ route('port04.page.content', ['PORT04PortfoliosCategory' => $portfolio->category->slug, 'PORT04Portfolios' => $portfolio->slug] ) }}">
-                                <img src="{{ asset('storage/' . $portfolio->path_image) }}" alt="Imagem do portfólio">
-                                <div>
-                                    @if ($portfolio->title)
-                                        <h4>{{$portfolio->title}}</h4>
-                                    @endif
-                                    @if ($portfolio->description)
-                                        <p>
-                                            {!! $portfolio->description !!}
-                                        </p>
-                                    @endif
+    {{-- BEGIN Page content --}}
+    <main id="root">
+        <section class="port04-page">
+            <header class="port04-page__banner container-fluid px-0">
+                <div class="port04-page__banner__container">
+                    @if ($section->active_banner)
+                        <div class="port04-page__banner__image"
+                            style="background-image: url({{ asset('storage/' . $section->path_image_desktop_banner) }});  background-color: {{ $section->background_color_banner }};">
+                            @if ($section->title_banner || $section->subtitle_banner)
+                                <div class="container port04-page__banner__header">
+                                    <h4 class="port04-page__banner__header__subtitle">{{ $section->subtitle_banner }}</h4>
+                                    <h3 class="port04-page__banner__header__title">{{ $section->title_banner }}</h3>
+                                    <hr class="port04-page__banner__header__line">
                                 </div>
-                                <img src="{{ asset('storage/' . $portfolio->path_image_icon) }}" alt="Ícone do portfólio">
-                            </a>
-                        </article>
-                    @endforeach
+                            @endif
+                        </div>
+                    @endif
                 </div>
-            @endif
-        </div>
-    </main>
-</section>
+            </header>
+            <main class="port04-page__portfolio">
+                <div class="container">
+                    <div class="port04-page__portfolio__header">
+                        @if ($section->active_content)
+                            @if ($section->subtitle_content || $section->title_content)
+                                <h2 class="port04-page__portfolio__header__subtitle">{{ $section->title_content }}</h2>
+                                <h1 class="port04-page__portfolio__header__title">{{ $section->subtitle_content }}</h1>
+                                <hr class="port04-page__portfolio__header__line">
+                            @endif
+                            @if ($section->text_content)
+                                <div class="port04-page__portfolio__header__description">
+                                    <p>
+                                        {!! $section->text_content !!}
+                                    </p>
+                                </div>
+                            @endif
+                        @endif
+                        @if ($categories->count())
+                            <nav class="port04-page__portfolio__categories">
+                                <ul class="port04-page__portfolio__categories__list w-100">
+                                    <div class="port04-page__portfolio__categories__carousel">
+                                        @foreach ($categories as $category)
+                                            <li
+                                                class=" {{ isset($category->selected) ? 'active' : '' }} port04-page__portfolio__categories__list__item">
+                                                <a class=" d-flex w-100 h-100 justify-content-between align-items-center"
+                                                    href="{{ route('port04.category.page', ['PORT04PortfoliosCategory' => $category->slug]) }}">
+                                                    @if ($category->path_image)
+                                                        <img src="{{ asset('storage/' . $category->path_image) }}"
+                                                            alt="Ícone da categoria"
+                                                            class="port04-page__portfolio__categories__list__item__icon">
+                                                    @endif
+                                                    {{ $category->title }}
+                                                </a>
+                                            </li>
+                                        @endforeach
+                                    </div>
+                                </ul>
+                            </nav>
+                        @endif
+                    </div>
+                    @if ($portfolios->count())
+                        <div class="port04-page__portfolio__content">
+                            <div class="port04-page__portfolio__content__carousel">
+                                @foreach ($portfolios as $portfolio)
+                                    <article class="port04-page__portfolio__content__item">
+                                        <a class="d-flex flex-row align-items-end w-100"
+                                            href="{{ route('port04.page.content', ['PORT04PortfoliosCategory' => $portfolio->category->slug, 'PORT04Portfolios' => $portfolio->slug]) }}">
+                                            <img class="port04-page__portfolio__content__item__image"
+                                                src="{{ asset('storage/' . $portfolio->path_image) }}"
+                                                alt="Imagem do portfólio">
+                                            <div class="port04-page__portfolio__content__item__container">
 
-{{-- Finish Content page Here --}}
-@foreach ($sections as $section)
-{!!$section!!}
-@endforeach
+                                                <div class="port04-page__portfolio__content__item__container__header">
+                                                    @if ($portfolio->title)
+                                                        <h4
+                                                            class="port04-page__portfolio__content__item__container__header__title">
+                                                            {{ $portfolio->title }}</h4>
+                                                    @endif
+                                                    @if ($portfolio->description)
+                                                        <p
+                                                            class="port04-page__portfolio__content__item__container__header__description">
+                                                            {!! $portfolio->description !!}
+                                                        </p>
+                                                    @endif
+                                                </div>
+                                                <img class="port04-page__portfolio__content__item__container__icon"
+                                                    src="{{ asset('storage/' . $portfolio->path_image_icon) }}"
+                                                    alt="Ícone do portfólio">
+                                            </div>
+
+                                        </a>
+                                    </article>
+                                @endforeach
+                            </div>
+
+                        </div>
+                    @endif
+                </div>
+            </main>
+        </section>
+        {{-- Finish Content page Here --}}
+        @foreach ($sections as $section)
+            {!! $section !!}
+        @endforeach
+    </main>
 @endsection
--- resources/views/Client/pages/Portfolios/PORT04/src/main.js
+++ resources/views/Client/pages/Portfolios/PORT04/src/main.js
@@ -1,3 +1,4 @@
+/* SECTION */
 var owlProtfolios = $('.port04__portfolios__carousel');
 owlProtfolios.addClass('owl-carousel')
 owlProtfolios.owlCarousel({
@@ -26,3 +27,66 @@ owlProtfolios.owlCarousel({
         }
     }
 });
+/* PAGE */
+/* BEGIN CATEGORIES */
+var owlCaterories = $('.port04-page__portfolio__categories__carousel');
+owlCaterories.addClass('owl-carousel')
+owlCaterories.owlCarousel({
+    loop:false,
+    nav: false,
+    dots:false,
+    margin:24,
+    rewind:true,
+    autoWidth: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        960:{
+            items:5
+        },
+        1200:{
+            items:4
+        },
+        2000:{
+            items:8
+        }
+    }
+});
+
+/* END CATEGORIES */
+
+
+/* BEGIN CONTENT */
+var owlCaterories = $('.port04-page__portfolio__content__carousel');
+owlCaterories.addClass('owl-carousel')
+owlCaterories.owlCarousel({
+    loop:false,
+    nav:false,
+    dots:true,
+    margin:24,
+    rewind:true,
+    autoWidth: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        960:{
+            items:5
+        },
+        1200:{
+            items:4
+        },
+        2000:{
+            items:8
+        }
+    }
+});
+
+/* END CONTENT */
