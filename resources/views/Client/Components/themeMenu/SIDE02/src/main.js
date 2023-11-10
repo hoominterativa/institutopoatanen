7b376a0316e244a790f02ffd2ac50e29ceb30738
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2023-11-10 12:07:31 -0300

    Update main.js

--- resources/views/Client/Components/themeMenu/SIDE02/src/main.js
+++ resources/views/Client/Components/themeMenu/SIDE02/src/main.js
@@ -14,15 +14,25 @@ $(function () {
         }
     });
 
-    // $('#SIDE02 ul li a').on('click', function(e){
-    //     e.preventDefault();
-    //     setTimeout(function(){
-    //         $('#side02').removeClass('side02--show');
-    //         $(this).removeClass('active');
-    //         $('.side02__override').removeClass('side02__override--show');
-    //         $('body').removeClass('no-scroll');
-    //     }, 1000)
-    // })
+    $('#SIDE02 ul li a[href^="#"]').on("click", function () {
+        var target = $(this.getAttribute("href"));
+        if (target.length) {
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+        }
+
+        setTimeout(function () {
+          $("#SIDE02").removeClass("side02--show");
+          $(this).removeClass("active");
+          $(".side02__override").removeClass("side02__override--show");
+          $("body").removeClass("no-scroll");
+        }, 1000);
+    });
+
     $(".side02__header__button-close").on("click", function () {
         $("#SIDE02").removeClass("side02--show");
         $(this).removeClass("active");
