240f62273ee7e3442788d73646eec0fc8cadfabb
Rodrigo - Front-end <70538582+rgomes2014@users.noreply.github.com>
2024-01-15 18:21:07 -0300

    ajuste rede social

--- resources/views/Client/Core/Headers/HEAD06/app.blade.php
+++ resources/views/Client/Core/Headers/HEAD06/app.blade.php
@@ -1,49 +1,61 @@
 <header class="head06">
     <menu class="head06__content">
-        <ul class="head06__content__left">
-            @for($i = 0; $i <= 3; $i++)
-                <li>Euuu</li>
-            @endfor
-        </ul>
+        <nav class="head06__content__left">
+            <ul class="head06__content__left__list">
+                <li class="head06__content__left__list__item">
+                    <a href="#">Home</a>
+                </li>
+                
+            </ul>
+        </nav>
         <div class="head06__content__logo">
             <a href="{{route('home')}}">
                 <img src="{{asset('storage/'.$generalSetting->path_logo_header_light)}}" alt="" width="202">
             </a>
         </div>
-        <ul class="head06__content__right">
-            @for($i = 0; $i <= 2; $i++)
-                <li>Euuu</li>
-            @endfor
-            <li class="list-inline-item menu-item dropdown">
-                <a href="#" target="_self" data-bs-toggle='dropdown' data-bs-toggle=jqueryanchor class="link transition">
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="sublink--menu text-end dropdown-menu" aria-labelledby="sublink--menu" >
-                    <div class="mb-2 dropdown">
-                        <a href="#" data-bs-toggle="dropdown" class="sublink-item transition">CTA <i class="menu-arrow"></i></a>
-                        <div class="dropdown-menu">
-                                <a href="#" class="sublink-item transition">Sub-item</a>
-                        </div>
+        <div class="head06__content__right">
+            <nav class="head06__content__right__menu">
+               <ul>
+                    @for($i = 0; $i <= 2; $i++)
+                        <li>Euuu</li>
+                    @endfor
+                    
+               </ul>
+            </nav>
+            
+            @if ($linksCtaHeader->count() && $callToActionTitle->active_header??false)
+                <div class="head06__content__right__cta">
+                    <div class="dropdown">
+                        @if ($linksCtaHeader->count()>1)
+                            <a href="javascript:void(0)" data-bs-toggle="dropdown" class="btn-cta transition">
+                                {{$callToActionTitle->title_header??''}}
+                                <i class="menu-arrow"></i>
+                            </a>
+                            <div class="sublink--cta-right text-end dropdown-menu" aria-labelledby="sublink--cta-right" >
+                                @foreach ($linksCtaHeader as $linkCtaHeader)
+                                    <a href="{{getUri($linkCtaHeader->link)}}" target="{{$linkCtaHeader->link_target}}" class="sublink-item">{{$linkCtaHeader->title}}</a>
+                                @endforeach
+                            </div>
+                        @else
+                            @foreach ($linksCtaHeader as $linkCtaHeader)
+                                <a href="{{getUri($linkCtaHeader->link)}}" target="{{$linkCtaHeader->link_target}}" class="btn-cta">{{$linkCtaHeader->title}}</a>
+                            @endforeach
+                        @endif
                     </div>
-                    <a href="#" class="sublink-item transition">CTA</a>
                 </div>
-            </li>
-            <li class="head06__content__right__cta">
-                <a href="#" class="link-full"></a>
-                CTA
-            </li>
-            <li class="head06__content__right__rede">
-                <a href="#">
-                    <img src="" alt="">
-                </a>
-                <a href="#">
-                    <img src="" alt="">
-                </a>
-                <a href="#">
-                    <img src="" alt="">
-                </a>
-            </li>
-        </ul>
+            @endif
+            {{-- END .btn-cta --}}
+            @if ($socials->count())
+                <div class="head06__content__right__rede">
+                    @foreach ($socials as $social)
+                    <a href="{{$social->link}}" title="{{$social->title}}">
+                        <img src="{{asset('storage/'.$social->path_image_icon)}}" alt="{{$social->title}}">
+                    </a>
+                    @endforeach
+                </div>
+            @endif
+            {{-- head06__content__right__rede --}}
+        </div>
     </menu>
 </header>
 {{-- END .btn-cta --}}
