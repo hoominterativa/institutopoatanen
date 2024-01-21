f785a94699da080d034cdc6cccf9729d1cec48bd
Rodrigo - Front-end <70538582+rgomes2014@users.noreply.github.com>
2024-01-21 16:49:36 -0300

    ajuste carrossel

--- resources/views/Client/pages/Contents/CONT14/src/main.js
+++ resources/views/Client/pages/Contents/CONT14/src/main.js
@@ -28,16 +28,35 @@ $('.cont13__left__link').on('click', function(e){
         type: "POST",
         url: url,
         success: function(response){
-            console.log(response);
-            
             $("#cont13__right__engBox").fadeOut(400, function(){
+               
+               
                 $(this).remove();
-                $(".cont13__right").append(response).fadeIn();
-            });
 
+              
+
+
+                $(".cont13__right").append(response).fadeIn();
+
+                  // Destruir o carrossel existente antes de recriá-lo
+                  $(".carousel-gallery-cont13").owlCarousel('destroy');
+                  $(".carousel-gallery-cont13").owlCarousel({
+                      items: 1,
+                      margin: 0,
+                      stagePadding: 0,
+                      smartSpeed: 450,
+                      autoplay: true,
+                      autoplayTimeout: 5000,
+                      loop: true,
+                      dots: true,
+                      nav: false,
+                  });
+                  // Acionar o evento refresh para garantir que o Owl Carousel seja atualizado corretamente
+                  $(".carousel-gallery-cont13").trigger('refresh.owl.carousel');
+            });
             $('.cont13__left__link').removeClass('active');
 
-            // Use the stored reference to 'this'
+       
             setTimeout(() => {
                 $this.addClass('active');
             }, 400);
@@ -48,37 +67,3 @@ $('.cont13__left__link').on('click', function(e){
         }
     });
 });
-// $('.cont13__left__link').on('click', function(e){
-//     e.preventDefault();
-
-//     const id = $(this).attr("id"); 
-//     const url = $(this).attr("url");
-
-//     $.ajax({
-//         headers: {
-//             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//         },
-//         type: "POST",
-//         url: url,
-//         data: { id: id },
-//         success: function(response){
-//             alert(response);
-
-//             // Fade out the existing content
-//             $("#cont13__right__engBox").fadeOut(400, function(){
-//                 $(this).empty();
-//                 $(".cont13__right").append(response).fadeIn();
-//             });
-
-//             $('.cont13__left__link').removeClass('active');
-
-//             setTimeout(() => {
-//                 $(this).addClass('active');
-//             }, 400);
-//         },
-//         error: function(xhr, status, error){
-//             console.error(error);
-//             console.log("Status Code:", xhr.status);
-//         }
-//     });
-// });
