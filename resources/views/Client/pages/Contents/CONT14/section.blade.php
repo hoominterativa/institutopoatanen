6ae7b035a375a5e7267534ef49695122eff4d630
Rodrigo - Front-end <70538582+rgomes2014@users.noreply.github.com>
2024-01-21 11:57:57 -0300

    HTML e css finish

--- resources/views/Client/pages/Contents/CONT14/section.blade.php
+++ resources/views/Client/pages/Contents/CONT14/section.blade.php
@@ -1,24 +1,28 @@
 <section id="cont13" class="cont13">
     <div class="container container--cont13">
-        <div class="row mx-auto">
-            <div class="cont13__left">
+        <div class="row row---cont13  mx-auto">
+            <div class="cont13__left col-auto px-0">
                 <h4 class="cont13__left__titleDest">Título Seção</h4>   
                 <nav class="cont13__left__navigation">
-                    <ul>
-                        <li><button>Título Categoria</button></li>
-                        <li><button>Título Categoria</button></li>
-                        <li><button>Título Categoria</button></li>
-                        <li><button>Título Categoria</button></li>
-                        <li><button>Título Categoria</button></li>
+                    <ul class="cont13__left__navigation__list">
+                        <li class="cont13__left__navigation__list__item"><button class="cont13__left__navigation__list__item__link">Título Categoria</button></li>
+                        <li class="cont13__left__navigation__list__item"><button class="cont13__left__navigation__list__item__link">Título Categoria</button></li>
+                        <li class="cont13__left__navigation__list__item"><button class="cont13__left__navigation__list__item__link">Título Categoria</button></li>
+                        <li class="cont13__left__navigation__list__item"><button class="cont13__left__navigation__list__item__link">Título Categoria</button></li>
+                        <li class="cont13__left__navigation__list__item"><button class="cont13__left__navigation__list__item__link">Título Categoria</button></li>
                     </ul>
                 </nav> 
             </div>
-            <div class="cont13__right">
+            <div class="cont13__right col">
                 <div class="cont13__right__box">
                     <h4 class="cont13__right__box__title">Título Conteúdo</h4>
                     <h5 class="cont13__right__box__subtitle">Subtítulo Conteúdo</h5>
-                    <div class="cont13__right__box__image">
-                        <img src="{{asset('storage/thumbnail-b.png')}}" alt="">
+                    <div class="carousel-gallery-cont13 owl-carousel">
+                        @for($i=0; $i <= 2; $i++)
+                        <div class="cont13__right__box__image">
+                            <img src="{{asset('storage/uploads/tmp/thumbnail-b.png')}}" class="h-100 w-100" alt="Imagem">
+                        </div>
+                        @endfor
                     </div>
                     <div class="cont13__right__box__paragraph">
                         <p>
---
+++ resources/views/Client/pages/Contents/CONT14/src/main.js
@@ -0,0 +1,11 @@
+$(".carousel-gallery-cont13").owlCarousel({
+    items: 1,
+    margin: 0,
+    stagePadding: 0,
+    smartSpeed: 450,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    loop: true,
+    dots: true,
+    nav: false,
+});
