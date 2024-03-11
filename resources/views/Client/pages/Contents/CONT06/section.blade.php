2c257b2f15b7fff529bc2b1d7bb5a8b9c3939022
DekiWill <William_Watanabe@hotmail.com>
2024-03-11 09:20:24 -0300

    Update: CONT06
    > Reestruturação do CONT06

--- resources/views/Client/pages/Contents/CONT06/section.blade.php
+++ resources/views/Client/pages/Contents/CONT06/section.blade.php
@@ -1,39 +1,50 @@
 @if ($contents->count())
     @foreach ($contents as $content)
-        <section id="CONT06" class="cont06 container-fluid px-0" style="background-image: url({{ asset('storage/' . $content->path_image_desktop) }}); background-color: {{ $content->background_color }};">
-            <div class="d-flex justify-content-center align-content-center flex-column">
-                @if ($content->title || $content->description)
-                    <header class="cont06__header">
-                        @if ($content->title)
-                            <h3 class="cont06__header__title">
-                                {{$content->title}}
-                                <hr class="cont06__header__line">
-                            </h3>
-                        @endif
-                        @if ($content->description)
-                            <p class="cont06__header__paragraph">{!! $content->description !!}</p>
-                        @endif
-                    </header>
-                @endif
-                <div class="cont06__boxVideo">
-                    @if ($content->link_video)
-                        <div class="cont06__boxVideo__content">
-                            <div id="videoApre" class="cont06__boxVideo__content__video d-flex justify-content-center align-items-center" data-src="{{getUri($content->link_video)}}" data-capa-video="{{ asset('storage/' . $content->path_image) }}" style="background-image: url({{ asset('storage/' . $content->path_image) }});">
-                                <img class="trans-fast play" src="{{ asset('storage/uploads/tmp/play.png') }}" alt="Play Vídeo">
-                            </div>
-                        </div>
+        <section class="cont06">
+
+            @if ($content->title || $content->description)
+                <header class="cont06__header">
+                    @if ($content->title)
+                        <h2 class="cont06__header__title">
+                            {{ $content->title }}
+                        </h2>
                     @endif
-                    @if ($content->link_button)
-                        <a href="{{ getUri($content->link_button) }}" target="{{ $content->target_link }}"
-                            class="cont06__boxVideo__cta transition d-flex justify-content-center align-items-center">
-                                <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="Ícone" class="cont06__cta__icon me-3 transition">
-                            @if ($content->title_button)
-                                {{ $content->title_button }}
-                            @endif
-                        </a>
+                    @if ($content->description)
+                        <div class="cont06__header__paragraph">{!! $content->description !!}</div>
                     @endif
-                </div>
-            </div>
+                </header>
+            @endif
+
+
+            @if ($content->link_video)
+                <main class="cont06__video" {{--BACKEND: INSERIR REQUIRED NO CAMPO DE IMAGEM THUMBNAIL --}}
+                    data-src="{{ getUri($content->link_video) }}"
+                    data-capa-video="{{ asset('storage/' . $content->path_image) }}"
+                    style="background-image: url({{ asset('storage/' . $content->path_image) }});">
+
+
+                    <button class="cont06__video__button" title="Play">
+                        <img class="cont06__video__button__icon" src="{{ asset('storage/uploads/tmp/play.png') }}"
+                            alt="Play Vídeo">
+                    </button>
+
+                </main>
+            @endif
+
+
+            @if ($content->link_button)
+                <a title="{{ $content->title_button }}" href="{{ getUri($content->link_button) }}"
+                    target="{{ $content->target_link }}" class="cont06__cta">
+
+                    @if ($content->title_button)
+                        {{ $content->title_button }}
+                    @endif
+                </a>
+            @endif
+
+
+
+
         </section>
     @endforeach
 @endif
--- resources/views/Client/pages/Contents/CONT06/src/main.js
+++ resources/views/Client/pages/Contents/CONT06/src/main.js
@@ -1,24 +1,52 @@
-if($("#videoApre").length){
-    var valorAntigo = $("#videoApre").attr('data-src');
-    var caminhoCapaVideo = $("#videoApre").attr('data-capa-video');
-    var arrValorAntigo = new Array();
-    if(valorAntigo.indexOf("/embed/") > 0){
-            if(valorAntigo != ""){
-                arrValorAntigo = valorAntigo.split("/embed/");
-                if(arrValorAntigo.length > 1){
-                        arrValorAntigo = arrValorAntigo[1].split('&');
-                        if(caminhoCapaVideo == ""){
-                                $("#videoApre").css('background-image', 'url(https://i.ytimg.com/vi/'+arrValorAntigo[0]+'/hqdefault.jpg)');
-                        }else{
-                                $("#videoApre").css('background-image', 'url('+caminhoCapaVideo+')');
-                        }
-                }
-            }
-    }
-    $('body').on('click', '#videoApre .play', function(){
-        $urlVideo = $(this).parents('#videoApre').attr('data-src');
-        $(this).parents('.cont06__boxVideo__content').append('<iframe id="urlYoutube" width="99.5%" height=500" src="'+$urlVideo+'?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-        $(this).parents('#videoApre').remove();
-    });
+// if($("#videoApre").length){
+//     var valorAntigo = $("#videoApre").attr('data-src');
+//     var caminhoCapaVideo = $("#videoApre").attr('data-capa-video');
+//     var arrValorAntigo = new Array();
 
+//     if(valorAntigo.indexOf("/embed/") > 0){ //só sobe link do yt então essa checagem se torna redundante;
+//         //caso haja um video embed do you tube e não tenha thumbnail vinda do painel é recupreamos a thumbnail do próprio youtube;
+//             if(valorAntigo != ""){
+//                 arrValorAntigo = valorAntigo.split("/embed/");
+//                 if(arrValorAntigo.length > 1){
+//                         arrValorAntigo = arrValorAntigo[1].split('&');
+//                         if(caminhoCapaVideo == ""){
+//                                 $("#videoApre").css('background-image', 'url(https://i.ytimg.com/vi/'+arrValorAntigo[0]+'/hqdefault.jpg)');
+//                         }else{
+//                                 $("#videoApre").css('background-image', 'url('+caminhoCapaVideo+')');
+//                         }
+//                 }
+//             }
+//     }
+
+
+
+
+// }
+
+// $('body').on('click', '.cont06__video__button', function(){
+//     $urlVideo = $(this).parents('.cont06__video').attr('data-src');
+//     // cria um ifrme com o vídeo do youtube
+//     $(this).parents('.cont06__video').append('<iframe id="urlYoutube" width="99.5%" height=500" src="'+$urlVideo+'?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+//     // $(this).parents('#cont06__video').remove();
+// });
+
+
+const buttonsPlay = document.querySelectorAll('.cont06__video__button');
+
+if(buttonsPlay) {
+    buttonsPlay.forEach(el =>{
+        el.addEventListener('click', () =>{
+            const src = el.parentNode.dataset.src;
+            const iframe = document.createElement('iframe');
+
+            iframe.setAttribute('src', src+'?autoplay=1');
+            iframe.classList.add('cont06__video__iframe');
+            iframe.setAttribute('id', 'urlYoutube');
+
+            el.style.display = 'none';
+
+            el.parentNode.appendChild(iframe);
+
+        })
+    })
 }
