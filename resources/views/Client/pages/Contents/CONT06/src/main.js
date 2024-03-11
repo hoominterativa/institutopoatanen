73727c2e0724435525ed73a5a22f06db2ba5e654
DekiWill <William_Watanabe@hotmail.com>
2024-03-11 09:24:06 -0300

    Update: Remoção do JS antigo

--- resources/views/Client/pages/Contents/CONT06/src/main.js
+++ resources/views/Client/pages/Contents/CONT06/src/main.js
@@ -1,36 +1,3 @@
-// if($("#videoApre").length){
-//     var valorAntigo = $("#videoApre").attr('data-src');
-//     var caminhoCapaVideo = $("#videoApre").attr('data-capa-video');
-//     var arrValorAntigo = new Array();
-
-//     if(valorAntigo.indexOf("/embed/") > 0){ //só sobe link do yt então essa checagem se torna redundante;
-//         //caso haja um video embed do you tube e não tenha thumbnail vinda do painel é recupreamos a thumbnail do próprio youtube;
-//             if(valorAntigo != ""){
-//                 arrValorAntigo = valorAntigo.split("/embed/");
-//                 if(arrValorAntigo.length > 1){
-//                         arrValorAntigo = arrValorAntigo[1].split('&');
-//                         if(caminhoCapaVideo == ""){
-//                                 $("#videoApre").css('background-image', 'url(https://i.ytimg.com/vi/'+arrValorAntigo[0]+'/hqdefault.jpg)');
-//                         }else{
-//                                 $("#videoApre").css('background-image', 'url('+caminhoCapaVideo+')');
-//                         }
-//                 }
-//             }
-//     }
-
-
-
-
-// }
-
-// $('body').on('click', '.cont06__video__button', function(){
-//     $urlVideo = $(this).parents('.cont06__video').attr('data-src');
-//     // cria um ifrme com o vídeo do youtube
-//     $(this).parents('.cont06__video').append('<iframe id="urlYoutube" width="99.5%" height=500" src="'+$urlVideo+'?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-//     // $(this).parents('#cont06__video').remove();
-// });
-
-
 const buttonsPlay = document.querySelectorAll('.cont06__video__button');
 
 if(buttonsPlay) {
