ed76a30bf04974f57df886e68138674a6156bc96
DekiWill <William_Watanabe@hotmail.com>
2024-03-14 10:34:10 -0300

    Update: SERV10
    > Finalização da reestruturação do SERV10

--- resources/views/Client/pages/Services/SERV10/show.blade.php
+++ resources/views/Client/pages/Services/SERV10/show.blade.php
@@ -17,7 +17,8 @@
         <section class="serv10-show__main">
             @if ($service->path_image)
                 <div class="serv10-show__main__image">
-                    <img src="{{ asset('storage/' . $service->path_image) }}" alt="Imagem Box">
+                    <img src="{{ asset('storage/' . $service->path_image) }}" alt="Imagem Box"
+                        class="serv10-show__main__image__img">
                 </div>
             @endif
 
@@ -29,9 +30,7 @@
 
                     @if ($service->text)
                         <div class="serv10-show__main__information__paragraph">
-                            <p>
-                                {!! $service->text !!}
-                            </p>
+                            {!! $service->text !!}
                         </div>
                     @endif
                 </div>
@@ -39,15 +38,19 @@
         </section>
 
         @if ($contents->count())
-            <div class="serv10-show__faq">
-                <div class="container container--faq">
-                    @if ($service->active_content == 1)
-                        <div class="serv10-show__faq__header">
-                            @if ($service->title_content || $service->subtitle_content)
+            <section class="serv10-show__faq">
+                @if ($service->active_content == 1)
+                    @if ($service->title_content || $service->subtitle_content || $service->description_content)
+                        <header class="serv10-show__faq__header">
+
+                            @if ($service->title_content)
                                 <h2 class="serv10-show__faq__header__title">{{ $service->title_content }}</h2>
-                                <h3 class="serv10-show__faq__header__subtitle">{{ $service->subtitle_content }}</h3>
-                                <hr class="serv10-show__faq__header__line">
                             @endif
+
+                            @if ($service->subtitle_content)
+                                <h3 class="serv10-show__faq__header__subtitle">{{ $service->subtitle_content }}</h3>
+                            @endif
+
                             @if ($service->description_content)
                                 <div class="serv10-show__faq__header__paragraph">
                                     <p>
@@ -55,120 +58,118 @@
                                     </p>
                                 </div>
                             @endif
-                        </div>
+
+                        </header>
                     @endif
-                    <div class="serv10-show__faq__main">
-                        @foreach ($contents as $content)
-                            <div class="serv10-show__faq__main__box">
-                                @if ($content->title)
-                                    <button class="serv10-show__faq__main__box__tab accordion-button collapsed"
-                                        type="button" data-bs-toggle="collapse" data-bs-target="#faq-{{ $content->id }}"
-                                        aria-expanded="false" aria-controls="collapseTwo">
-                                        <h4 class="serv10-show__faq__main__box__tab__title">{{ $content->title }}</h4>
-                                    </button>
-                                @endif
-                                @if ($content->description)
-                                    <div id="faq-{{ $content->id }}"
-                                        class="serv10-show__faq__main__box__description accordion-collapse collapse"
-                                        data-bs-parent="#faq-{{ $content->id }}">
-                                        <div class="serv10-show__faq__main__box__description__paragraph">
-                                            <p>
-                                                {!! $content->description !!}
-                                            </p>
-                                        </div>
-                                    </div>
-                                @endif
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
+                @endif
+
+                <main class="serv10-show__faq__main">
+                    @foreach ($contents as $content)
+                        <details class="serv10-show__faq__main__item">
+                            <summary class="serv10-show__faq__main__item__title" aria-level="3" role="heading">
+                                {{ $content->title }}
+                            </summary>
+
+                            @if ($content->description)
+                                <div class="serv10-show__faq__main__item__paragraph details-content">
+                                    <p>
+                                        {!! $content->description !!}
+                                    </p>
+                                </div>
+                            @endif
+
+                        </details>
+                    @endforeach
+                </main>
+
+            </section>
         @endif
 
         @if ($topics->count())
-            <div class="serv10-show__featuredBox">
-                <div class="container container--feat">
-                    @if ($service->active_topic == 1)
-                        <div class="serv10-show__featuredBox__header">
-                            @if ($service->title_topic || $service->subtitle_topic)
-                                <h2 class="serv10-show__featuredBox__header__title">{{ $service->title_topic }}</h2>
-                                <h3 class="serv10-show__featuredBox__header__subtitle">{{ $service->subtitle_topic }}</h3>
-                                <hr class="serv10-show__featuredBox__header__line">
+            <section class="serv10-show__topics">
+                @if ($service->active_topic == 1)
+                    @if ($service->title_topic || $service->subtitle_topic || $service->description_topic)
+                        <header class="serv10-show__topics__header">
+                            @if ($service->title_topic)
+                                <h2 class="serv10-show__topics__header__title">{{ $service->title_topic }}</h2>
                             @endif
+
+                            @if ($service->subtitle_topic)
+                                <h3 class="serv10-show__topics__header__subtitle">{{ $service->subtitle_topic }}</h3>
+                            @endif
+
                             @if ($service->description_topic)
-                                <div class="serv10-show__featuredBox__header__paragraph">
+                                <div class="serv10-show__topics__header__paragraph">
                                     <p>
                                         {!! $service->description_topic !!}
                                     </p>
                                 </div>
                             @endif
-                        </div>
+                        </header>
                     @endif
-                    <div class="serv10-show__featuredBox__main row mx-auto">
-                        @foreach ($topics as $topic)
-                            <div class="serv10-show__featuredBox__main__box col-sm-3">
-                                <div class="serv10-show__featuredBox__main__box__content">
-                                    @if ($topic->path_image)
-                                        <div class="serv10-show__featuredBox__main__box__bg">
-                                            <img src="{{ asset('storage/' . $topic->path_image) }}" alt="Bg">
-                                        </div>
-                                    @endif
-                                    <div class="serv10-show__featuredBox__main__box__description">
-                                        @if ($topic->path_image_icon)
-                                            <div class="serv10-show__featuredBox__main__box__description__icon">
-                                                <img src="{{ asset('storage/' . $topic->path_image_icon) }}"
-                                                    alt="ícone">
-                                            </div>
-                                        @endif
-                                        @if ($topic->title)
-                                            <h4 class="serv10-show__featuredBox__main__box__description__title">
-                                                {{ $topic->title }}</h4>
-                                        @endif
-                                        @if ($topic->description)
-                                            <div class="serv10-show__featuredBox__main__box__description__paragraph">
-                                                <p>
-                                                    {!! $topic->description !!}
-                                                </p>
-                                            </div>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            {{-- END serv10-show__featuredBox__main__box --}}
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        @endif
+                @endif
 
-        @if ($galleries->count())
-            <div class="serv10-show__gallery">
-                <div class="container container--gall">
-                    @if ($service->active_gallery == 1)
-                        <div class="serv10-show__gallery__header">
-                            @if ($service->title_gallery)
-                                <h2 class="serv10-show__gallery__header__title">{{ $service->title_gallery }}</h2>
+                <main class="serv10-show__topics__main">
+                    @foreach ($topics as $topic)
+                        <div class="serv10-show__topics__main__item">
+
+                            @if ($topic->path_image)
+                                <img class="serv10-show__topics__main__item__bg"
+                                    src="{{ asset('storage/' . $topic->path_image) }}"
+                                    alt="background do tópico {{ $topic->title }}">
                             @endif
-                            @if ($service->description_gallery)
-                                <div class="serv10-show__gallery__header__paragraph">
+
+                            @if ($topic->path_image_icon)
+                                <img src="{{ asset('storage/' . $topic->path_image_icon) }}" alt="ícone">
+                            @endif
+
+                            @if ($topic->title)
+                                <h4 class="serv10-show__featuredBox__main__box__description__title">
+                                    {{ $topic->title }}</h4>
+                            @endif
+
+                            @if ($topic->description)
+                                <div class="serv10-show__featuredBox__main__box__description__paragraph">
                                     <p>
-                                        {!! $service->description_gallery !!}
+                                        {!! $topic->description !!}
                                     </p>
                                 </div>
                             @endif
                         </div>
-                    @endif
-                    <div class="serv10-show__gallery__main row mx-auto">
-                        @foreach ($galleries as $gallery)
-                            <div class="serv10-show__gallery__main__box col-sm-3">
-                                <figure class="serv10-show__gallery__main__box__content">
-                                    <img src="{{ asset('storage/' . $gallery->path_image) }}" alt="Imagem Galeria">
-                                </figure>
+                    @endforeach
+                </main>
+
+            </section>
+        @endif
+
+        @if ($galleries->count())
+            <div class="serv10-show__gallery">
+
+                @if ($service->active_gallery == 1)
+                    <header class="serv10-show__gallery__header">
+                        @if ($service->title_gallery)
+                            <h2 class="serv10-show__gallery__header__title">{{ $service->title_gallery }}</h2>
+                        @endif
+
+                        @if ($service->description_gallery)
+                            <div class="serv10-show__gallery__header__paragraph">
+                                <p>
+                                    {!! $service->description_gallery !!}
+                                </p>
                             </div>
-                        @endforeach
-                        {{-- END serv10-show__gallery__main__box --}}
-                    </div>
-                </div>
+                        @endif
+                    </header>
+                @endif
+
+                <main class="serv10-show__gallery__main">
+                    @foreach ($galleries as $gallery)
+                        <img
+                        class="serv10-show__gallery__main__item"
+                        src="{{ asset('storage/' . $gallery->path_image) }}"
+                        alt="Imagem Galeria">
+                    @endforeach
+                </main>
+
             </div>
         @endif
 
