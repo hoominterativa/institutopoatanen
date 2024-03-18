8f6cb0d06b0ca0025e1b97cf795ca1d6cdfde5c2
DekiWill <William_Watanabe@hotmail.com>
2024-03-18 10:13:33 -0300

    Update: Ajustes de refino

--- resources/views/Client/pages/Galleries/GALL02/show.blade.php
+++ resources/views/Client/pages/Galleries/GALL02/show.blade.php
@@ -11,18 +11,11 @@
         </header>
     @endif
 
-
-    {{-- <figure class="gall02-show__gallery">
-        <img src="{{ asset('storage/' . $gallery->path_image) }}" class="gall02-show__gallery__item"
-            alt="{{ $gallery->image_legend }}">
-        <figcaption class="gall02-show__gallery__item__legend">{{ $gallery->image_legend }}</figcaption>
-    </figure> --}}
-
     <div class="gall02-show__gallery">
         <figure class="gall02-show__gallery__swipper-wrapper swiper-wrapper">
             @foreach ($gallery->images as $image)
-                <img class="gall02-show__gallery__item swiper-slide " src="{{ asset('storage/' . $image->path_image) }}"
-                alt="{{ $gallery->image_legend }}">
+                <img class="gall02-show__gallery__item swiper-slide" loading="lazy"
+                    src="{{ asset('storage/' . $image->path_image) }}" alt="{{ $gallery->image_legend }}">
             @endforeach
             <figcaption class="gall02-show__gallery__description">
                 {{ $gallery->image_legend }}
@@ -31,11 +24,13 @@
         <div class="gall02-show__gallery__swiper-button-prev swiper-button-prev"></div>
         <div class="gall02-show__gallery__swiper-button-next swiper-button-next"></div>
     </div>
+
+
     <div class="gall02-show__thumbs">
         <div class="gall02-show__thumbs__swiper-wrapper swiper-wrapper">
             @foreach ($gallery->images as $image)
-                <img src="{{ asset('storage/' . $image->path_image) }}"
-                    class="gall02-show__thumbs__item swiper-slide"  alt="{{ $gallery->image_legend }}">
+                <img src="{{ asset('storage/' . $image->path_image) }}" loading="lazy"
+                    class="gall02-show__thumbs__item swiper-slide" alt="{{ $gallery->image_legend }}">
             @endforeach
         </div>
 
