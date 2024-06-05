55053772b83c952540555886a0fd8d92c29e5788
ia5go <benvindoiago@outlook.com>
2024-06-05 09:26:23 -0300

    modelsConfig

--- resources/views/Client/pages/Contents/CONT05/section.blade.php
+++ resources/views/Client/pages/Contents/CONT05/section.blade.php
@@ -1,35 +1,37 @@
 @if ($contents->count())
     @foreach ($contents as $content)
-        <section id="CONT05" class="cont05 container-fluid px-0"
-        style="background-image: url({{ asset('storage/' . $content->path_image_desktop) }}); background-color: {{ $content->background_color }};">
-            <div class="d-flex justify-content-center align-content-center flex-column">
-                <header class="cont05__header">
-                    @if ($content->title)
-                        <h3 class="cont05__header__title">
-                            {{$content->title}}
-                        </h3>
-                        <hr class="cont05__header__line">
-                    @endif
-                    @if ($content->description)
-                        <p class="cont05__header__paragraph">{!! $content->description !!}</p>
-                    @endif
-                    @if ($content->subtitle)
-                        <h2 class="subtitle">{{$content->subtitle}}</h2>
-                    @endif
-                </header>
-                <div class="cont05__content">
-                    @if ($content->link_button)
-                    <a href="{{getUri($content->link_button)}}" target="{{$content->target_link_button}}" class="cont05__content__cta transition d-flex justify-content-center align-items-center">
-                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="ìcone" class="cont05__cta__icon me-3 transition">
+        <section id="CONT05" class="cont05" {{-- style="background-image: url({{ asset('storage/' . $content->path_image_desktop) }}); background-color: {{ $content->background_color }};" --}}>
+
+            <header class="cont05__header">
+                @if ($content->title)
+                    <h2 class="cont05__header__title">
+                        {{ $content->title }}
+                    </h2>
+                @endif
+
+                @if ($content->subtitle)
+                    <h3 class="subtitle">{{ $content->subtitle }}</h3>
+                @endif
+
+                @if ($content->description)
+                    <p class="cont05__header__paragraph">{!! $content->description !!}</p>
+                @endif
+
+            </header>
+
+            <div class="cont05__content">
+                @if ($content->link_button)
+                    <a href="{{ getUri($content->link_button) }}" target="{{ $content->target_link_button }}"
+                        class="cont05__content__cta transition d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('storage/uploads/tmp/icon-general.svg') }}" alt="ìcone"
+                            class="cont05__cta__icon me-3 transition">
                         @if ($content->title_button)
                             {{ $content->title_button }}
                         @endif
                     </a>
-                    @endif
-                </div>
+                @endif
             </div>
+
         </section>
     @endforeach
 @endif
-
-{{-- END #CONT05 --}}
