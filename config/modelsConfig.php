ffbb6bcf1014b05282af63af35f4a9ec151dae31
ia5go <benvindoiago@outlook.com>
2024-07-24 16:29:40 -0300

    compliance
    atualizado;
    models config tbm atualizado;

--- config/modelsConfig.php
+++ config/modelsConfig.php
@@ -15,7 +15,7 @@ return [
     // Configure existing modules and templates site-wide/system
     'InsertModelsMain' => (object) [
         'Slides' => (object) [
-            'SLID02' => (object)[
+            'SLID01' => (object)[
                 'ViewHome' => true,
                 'ViewListMenu' => false,
                 'ViewListPanel' => true,
