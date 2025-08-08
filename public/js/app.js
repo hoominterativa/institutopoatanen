f18406412a46da160389be8d9f46855eed757ac8
Abne Mescena <136840163+MescenaFront@users.noreply.github.com>
2025-08-08 18:28:27 -0300

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -11753,17 +11753,16 @@ $(document).ready(function () {
   \****************************************************************/
 /***/ (() => {
 
-// const header = document.querySelector(".head02");
-
-// if (header) {
-//     window.addEventListener("scroll", function () {
-//         if (window.scrollY > 50) {
-//             header.classList.add("scrolled");
-//         } else {
-//             header.classList.remove("scrolled");
-//         }
-//     });
-// }
+var header = document.querySelector(".head02");
+if (header) {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+}
 
 /***/ }),
 
